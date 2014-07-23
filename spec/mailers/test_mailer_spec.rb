65c1b3cf812e9f572bc8c03e0799990f9fe84a27
Abigail Jones <AbbyJonesDev@gmail.com>
2014-07-23 09:10:12 -0500

    Raise errors if email doesn't belong to campaign or campaign doesn't belong to category.  Specs passing but need to be refactored.

--- spec/mailers/test_mailer_spec.rb
+++ spec/mailers/test_mailer_spec.rb
@@ -5,9 +5,13 @@ describe TestMailer do
   describe 'email_one_person' do
 
     before :each do
+      category = EmailCampaignCategory.new
+      category.name = "Testing"
+      category.save
       campaign = EmailCampaign.new
       campaign.name = "Test"
       campaign.method_name = "TestMailer.email_one_person"
+      campaign.email_campaign_category = category
       campaign.save
       FactoryGirl.create(:user, :email => "foo@bar.com")
       TestMailer.email_one_person
@@ -29,6 +33,26 @@ describe TestMailer do
     it "records the email after it is sent" do
       expect(SentEmail.last.user).to eq(User.last)
     end
+
+    describe "check campaign" do
+      #  "Happy path" covered above 
+      #  Email should be delivered if it belongs to a campaign and category.
+
+      it "alerts if email doesn't belong to campaign" do
+        EmailCampaign.destroy_all
+        expect { TestMailer.email_one_person }.to raise_error(TestMailer::NoCampaignError) 
+      end
+
+      it "alerts if email campaign doesn't belong to category" do
+        campaign = EmailCampaign.new
+        campaign.name = "Test"
+        campaign.method_name = "TestMailer.email_one_person"
+        campaign.save
+        EmailCampaignCategory.destroy_all
+        expect { TestMailer.email_one_person }.to raise_error(TestMailer::NoCampaignCategoryError)
+      end
+    end 
+
 
   end
 
