102b6d1b238c0b60f4835e98669dd83d76f40efc
Abigail Jones <AbbyJonesDev@gmail.com>
2014-08-08 17:40:05 -0500

    Create Unsubscription model method for unsubscribing to email campaign categories.

--- app/models/unsubscription.rb
+++ app/models/unsubscription.rb
@@ -1,2 +1,15 @@
 class Unsubscription < ActiveRecord::Base
+  validates :user_id, :email_campaign_category_id, presence: true
+  validates_uniqueness_of :user_id, :scope => :email_campaign_category_id
+  attr_accessible :user_id, :email_campaign_category_id
+
+  def self.unsubscribe(user, category_names)
+    ecc_list = []
+    category_names.each do |c|
+      ecc_list << EmailCampaignCategory.find_by_name(c)
+    end
+    ecc_list.each do |c|
+      Unsubscription.create(user_id: user.id, email_campaign_category_id: c.id)
+    end
+  end
 end
--- spec/models/unsubscription_spec.rb
+++ spec/models/unsubscription_spec.rb
@@ -1,20 +1,51 @@
 require 'spec_helper'
 
 describe Unsubscription do
+    let!(:user)  { FactoryGirl.create(:user) }
+    let!(:category1) { FactoryGirl.create(:email_campaign_category) }
+    let!(:category2) { FactoryGirl.create(:email_campaign_category) }
+    let!(:category3) { FactoryGirl.create(:email_campaign_category) }
+    let!(:category4) { FactoryGirl.create(:email_campaign_category) }
+
+
+  it "is not valid without user id" do
+    u = Unsubscription.new
+    u.email_campaign_category_id = category1.id
+    expect(u.valid?).to be_false
+  end
+
+  it "is not valid without email_campaign_category id" do
+    u = Unsubscription.new
+    u.user_id = user.id
+    expect(u.valid?).to be_false
+  end
 
   describe '#unsubscribe' do
 
     it 'does not create duplicate unsubscriptions' do
-      pending
+      Unsubscription.unsubscribe(user, [category1.name])
+      expect{
+        Unsubscription.unsubscribe(user, [category1.name])
+      }.to change{Unsubscription.count}.by(0)
     end
 
     it "creates one new unsubscription" do
-      pending
+      expect { 
+        Unsubscription.unsubscribe(user, [category2.name])
+        }.to change{Unsubscription.count}.by(1)
     end
 
     it "creates multiple unsubscriptions" do
-      pending
+      expect { 
+        Unsubscription.unsubscribe(user, [category3.name, category4.name])
+        }.to change{Unsubscription.count}.by(2)
+    end
+
+    it "creates multiple unsubscriptions without duplicates" do
+      Unsubscription.create(user_id: user.id, email_campaign_category_id: category1.id)
+      expect { 
+        Unsubscription.unsubscribe(user, [category1.name, category3.name, category4.name])
+        }.to change{Unsubscription.count}.by(2)
     end
   end
-
 end
