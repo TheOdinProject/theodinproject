3070cf39a8f2276e54970f46fee558e053b26a9d
Abigail Jones <AbbyJonesDev@gmail.com>
2015-03-08 20:13:44 -0500

    Testing Mail Helper in progress

--- spec/helpers/mail_campaign_helper_spec.rb
+++ spec/helpers/mail_campaign_helper_spec.rb
@@ -2,5 +2,31 @@ require 'spec_helper'
 
 describe MailCampaignHelper do
 
+  describe '#record(campaign, recipients)' do
+    before do 
+      3.times { FactoryGirl.create(:user) }
+      recipients = User.all
+      campaign = FactoryGirl.create(:email_campaign)
+      record(campaign, recipients)
+    end
+
+    it 'creates SentEmail records for each recipient' do
+      expect(SentEmail.count).to eq(3)
+    end
+
+    specify 'SentEmail matches campaign' do
+      expect(SentEmail.first.email_campaign).to eq(EmailCampaign.first)
+    end
+
+    specify 'SentEmail matches recipients' do
+      expect(SentEmail.first.user).to eq(User.first)
+    end
+  end
+
+  describe '#build_header(recipients)' do
+    it 'returns creates SMTPAPI header for Sendgrid' 
+    # Best way to test is with Sendgrid validator
+    # https://sendgrid.com/docs/API_Reference/SMTP_API/smtpapi_validator.html   
+  end
 
 end
