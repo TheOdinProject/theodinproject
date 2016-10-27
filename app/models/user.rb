e12c221d259728d8d1eab96be19dbf48cb324f0b
Jack Huang <jack7100@gmail.com>
2016-10-27 13:26:07 -0400

    Adjust models to inherit from ApplicationRecord instead of ActiveRecord::Base

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
