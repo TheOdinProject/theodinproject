b362801d5e0445ab56707d06547045084bd688b8
Chelsi Jones <cajones.bz@gmail.com>
2017-09-03 10:17:32 -0500

    Add display method for rest of world

--- gitpro.rb
+++ gitpro.rb
@@ -1,5 +1,6 @@
 class Date
 	attr_accessor :year, :month, :day
+	@@months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 	def initialize(year=nil, month=nil, day=nil)
 		today = Time.new
@@ -8,11 +9,16 @@ class Date
 		day.nil? ? @day = today.day : @day = day
 	end
 
-	def display
-		months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-		puts "#{months[@month-1]} #{@day}, #{@year}"
+	def display_usa
+		puts "#{@@months[@month-1]} #{@day}, #{@year}"
+	end
+
+	def display_int
+		puts "#{@day} #{@@months[@month-1]} #{@year}"
 	end
 end
 
+
 #date = Date.new(2018, 3, 8)
-#puts date.display
+#puts date.display_usa
+#puts date.display_int
