c5927c21df40752277972a297ac5825c3c3e9eef
Chelsi Jones <cajones.bz@gmail.com>
2017-09-03 10:06:54 -0500

    Add display method

--- gitpro.rb
+++ gitpro.rb
@@ -1,8 +1,18 @@
 class Date
-	def initialize(year, month, day)
+	attr_accessor :year, :month, :day
+
+	def initialize(year=nil, month=nil, day=nil)
 		today = Time.new
-		@year = year or today.year
-		@month = month or today.month
-		@day = day or today.day
+		year.nil? ? @year = today.year : @year = year
+		month.nil? ? @month = today.month : @month = month
+		day.nil? ? @day = today.day : @day = day
+	end
+
+	def display
+		months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+		puts "#{months[@month-1]} #{@day}, #{@year}"
 	end
 end
+
+#date = Date.new(2018, 3, 8)
+#puts date.display
