5589455f38e3e3e1d34edfd2cbbed8dabf951087
Rhys <105ron@users.noreply.github.com>
2017-10-25 07:07:21 +1100

    Update lesson.rb

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -57,8 +57,7 @@ class Lesson < ApplicationRecord
   def github_response
     Octokit.contents(
       'theodinproject/curriculum',
-      path: url,
-      ref: 'master'
+      path: url
     )
   end
 
