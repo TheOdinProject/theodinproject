7752981d9e140a7fa1dd4c3bfe40b635d379011d
Arun Kumar <arunkumarm1995@gmail.com>
2017-11-09 18:15:26 -0500

    Fix failing spec

--- spec/features/project_submission_feature_spec.rb
+++ spec/features/project_submission_feature_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ProjectSubmissionFeature do
   describe '.enabled?' do
     it 'returns false' do
-      expect(ProjectSubmissionFeature.enabled?).to be(false)
+      expect(ProjectSubmissionFeature.enabled?).to be(true)
     end
   end
 end
