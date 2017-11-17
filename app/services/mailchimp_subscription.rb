2a2b034635e831bb09e01b8238e04bf7ad9f6f35
Bideo Wego <bideowego@gmail.com>
2017-11-17 17:38:18 -0500

    Update mailchimp subscription service and test helper

--- app/services/mailchimp_subscription.rb
+++ app/services/mailchimp_subscription.rb
@@ -1,48 +1,43 @@
 class MailchimpSubscription
+  attr_reader :email, :username, :signup_date
+  private :email, :username, :signup_date
 
-  # Creates a member on the mailchimp mailing list with the given options
-  #
-  # @param [Hash] options The attributes of the mailing list member
-  # @option options [String] :email The member's email
-  # @option options [String] :username The member's username
-  # @option options [String] :signup_date The date at which the member signed up or registered
-  # @return [Gibbon::Response] The response from the mailchimp API
+  def initialize(options)
+    @email = options[:email]
+    @username = options[:username]
+    @signup_date = options[:signup_date]
+  end
+
   def self.create(options)
-    email = options[:email]
-    username = options[:username]
-    signup_date = options[:signup_date]
+    new(options).create
+  end
 
-    merge_fields = {
-      USERNAME: username,
-      SIGNUPDATE: signup_date
-    }
-
-    create_request
-      .lists(ENV['MAILCHIMP_LIST_ID'])
-      .members
-      .create(
-        body: {
-          email_address: email,
-          status: :subscribed,
-          merge_fields: merge_fields
-        }
-      )
+  def create
+    mailchimp_list.members.create(body: subscriber_details)
   end
 
   private
 
-  # Creates a new mailchimp request
-  # @return [Gibbon::Request] The request
-  def self.create_request
-    Gibbon::Request.new(
-      api_key: ENV['MAILCHIMP_API_KEY']
-    )
+  def mailchimp_list
+    request.lists(ENV['MAILCHIMP_LIST_ID'])
+  end
+
+  def subscriber_details
+    {
+      email_address: email,
+      status: :subscribed,
+      merge_fields: merge_fields
+    }
+  end
+
+  def merge_fields
+    {
+      USERNAME: username,
+      SIGNUPDATE: signup_date
+    }
+  end
+
+  def request
+    Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
   end
 end
-
-
-
-
-
-
-
--- spec/support/mailchimp_helper.rb
+++ spec/support/mailchimp_helper.rb
@@ -1,66 +1,37 @@
 module MailchimpHelper
-  # Helper function to create a Gibbon request
   def mailchimp_create_request
-    Gibbon::Request.new(
-      api_key: ENV['MAILCHIMP_API_KEY']
-    )
+    Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
   end
 
-  # Get all members in list
   def mailchimp_get_members(list_id)
-    mailchimp_create_request
-      .lists(list_id)
-      .members
-      .retrieve
+    response = mailchimp_create_request.lists(list_id).members.retrieve
+    response.body['members']
   end
 
-  # Check if a member exists in a list by email
   def mailchimp_member_exists?(email, list_id)
-    response = mailchimp_get_members(list_id)
-    members = response.body['members']
+    members = mailchimp_get_members(list_id)
     members.any? { |member| member['email_address'] == email }
   end
 
-  # Helper function to remove a member from a list
-  # only if the member exists in that list
   def mailchimp_remove_member(email, list_id)
-    # Get list members from API
-    response = mailchimp_get_members(list_id)
-
-    emails = []
-    members = response.body['members']
-    members.each { |member| emails << member['email_address'] }
-
-    # Is the email in the list?
-    if emails.include?(email)
-
-      # MailChimp uses the MD5 hash of a user's email as their ID
-      md5 = Digest::MD5.new
-      member_id = md5.hexdigest(email)
-
-      # Delete the member
-      mailchimp_delete_member(list_id, member_id)
-    end
+    emails = mailchimp_list_member_emails(list_id)
+    mailchimp_delete_member(list_id, email) if emails.include?(email)
   end
 
   private
 
-  # Submits the delete request to remove a member from a list
-  def mailchimp_delete_member(list_id, member_id)
-    mailchimp_create_request
-      .lists(list_id)
-      .members(member_id)
-      .delete
+  def mailchimp_delete_member(list_id, email)
+    member_id = mailchimp_hash_email(email)
+    mailchimp_create_request.lists(list_id).members(member_id).delete
+  end
+
+  def mailchimp_hash_email(email)
+    md5 = Digest::MD5.new
+    md5.hexdigest(email)
+  end
+
+  def mailchimp_list_member_emails(list_id)
+    members = mailchimp_get_members(list_id)
+    members.map { |member| member['email_address'] }
   end
 end
-
-
-
-
-
-
-
-
-
-
-
