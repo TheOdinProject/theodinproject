1ffd8336ad3b999052a9403576f0a2e140cb0819
Arun Kumar <arunkumarm1995@gmail.com>
2018-01-01 18:02:49 -0500

    Fix votes controller spec errors

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -2,12 +2,7 @@ require 'rails_helper'
 
 RSpec.describe VotesController do
 
-  let(:project) {
-    double(
-      'Project',
-      total_votes: '2'
-    )
-  }
+  let(:project) { double('Project', user: other_user) }
 
   let(:params) {
     {
@@ -17,12 +12,12 @@ RSpec.describe VotesController do
   }
 
   let(:user) { double('User') }
+  let(:other_user) { double('User') }
   let(:project_id) { '1' }
   let(:user_signed_in?) { true }
 
   before do
-    allow(Project).to receive(:find).with(project_id).
-      and_return(project)
+    allow(Project).to receive(:find).with(project_id).and_return(project)
 
     allow(controller).to receive(:current_user).and_return(user)
     allow(controller).to receive(:user_signed_in?).and_return(user_signed_in?)
@@ -32,7 +27,7 @@ RSpec.describe VotesController do
 
   describe '#create' do
     it 'creates a new user vote for the project' do
-      post :create, params: params
+      post :create, params: params, format: :js
       expect(project).to have_received(:upvote_for).with(user)
     end
 
@@ -40,15 +35,23 @@ RSpec.describe VotesController do
       let(:user_signed_in?) { false }
 
       it 'does not create a new vote for the project' do
-        post :create, params: params
+        post :create, params: params, format: :js
         expect(project).to_not have_received(:upvote_for).with(user)
+      end
+    end
+
+    context 'returns bad request when user tries to vote his own project' do
+      before do
+        allow(controller).to receive(:current_user).and_return(:other_user)
+        post :create, params: params, format: :js
+        expect(response).to have_http_status(:bad_request)
       end
     end
   end
 
   describe '#destroy' do
     it 'removes the users vote for the project' do
-      delete :destroy, params: params
+      delete :destroy, params: params, format: :js
       expect(project).to have_received(:remove_vote_for).with(user)
     end
 
@@ -56,7 +59,7 @@ RSpec.describe VotesController do
       let(:user_signed_in?) { false }
 
       it 'does not create a new vote for the project' do
-        delete :destroy, params: params
+        delete :destroy, params: params, format: :js
         expect(project).to_not have_received(:remove_vote_for).with(user)
       end
     end
