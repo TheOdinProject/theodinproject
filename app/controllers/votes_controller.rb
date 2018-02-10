e6830c263d74df431731e5a92f6a822061b4fb24
Arun Kumar <arunkumarm1995@gmail.com>
2018-02-10 10:11:57 -0500

    Refactor votes controller and adjust specs

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,27 +3,19 @@ class VotesController < ApplicationController
 
   def create
     @project = Project.find(params[:project_id])
-    check_self_voting
+    return head :bad_request if @project.user == current_user
     @project.upvote_for(current_user)
     @decorated_project = ProjectDecorator.new(@project)
   end
 
   def destroy
     @project = Project.find(params[:project_id])
-    check_self_voting
+    return head :bad_request if @project.user == current_user
     @project.remove_vote_for(current_user)
     @decorated_project = ProjectDecorator.new(@project)
   end
 
   private
-
-  def check_self_voting
-    head :bad_request if @project.user == current_user
-  end
-
-  def set_project
-    @project = Project.find(params[:project_id])
-  end
 
   def authenticate_request
     head :unauthorized unless user_signed_in?
--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe VotesController do
-
+  let(:user) { double('User') }
+  let(:other_user) { double('Other User') }
   let(:project) { double('Project', user: other_user) }
 
+  let(:project_id) { '1' }
   let(:params) {
     {
       lesson_id: '1',
@@ -11,57 +13,73 @@ RSpec.describe VotesController do
     }
   }
 
-  let(:user) { double('User') }
-  let(:other_user) { double('User') }
-  let(:project_id) { '1' }
-  let(:user_signed_in?) { true }
-
-  before do
-    allow(Project).to receive(:find).with(project_id).and_return(project)
-    allow(controller).to receive(:current_user).and_return(user)
-    allow(controller).to receive(:user_signed_in?).and_return(user_signed_in?)
-    allow(project).to receive(:upvote_for).with(user)
-    allow(project).to receive(:remove_vote_for).with(user)
-  end
-
   describe '#create' do
-    context "user votes another user's project" do
-      it 'creates a new user vote for the project' do
-        post :create, params: params, format: :js
-        expect(project).to have_received(:upvote_for).with(user)
+    context 'when user is signed in' do
+      context "user votes another user's project" do
+        before do
+          allow(controller).to receive(:user_signed_in?).and_return(true)
+          allow(Project).to receive(:find).with(project_id).and_return(project)
+          allow(controller).to receive(:current_user).and_return(user)
+          allow(project).to receive(:upvote_for).with(user)
+        end
+
+        it 'creates a new user vote for the project' do
+          post :create, params: params, format: :js
+          expect(project).to have_received(:upvote_for).with(user)
+        end
+      end
+
+      context 'user tries to vote his/her own project' do
+        before do
+          allow(controller).to receive(:user_signed_in?).and_return(true)
+          allow(Project).to receive(:find).with(project_id).and_return(project)
+          allow(controller).to receive(:current_user).and_return(other_user)
+        end
+
+        it 'returns a status of bad request' do
+          post :create, params: params, format: :js
+          expect(response).to have_http_status(:bad_request)
+        end
       end
     end
 
     context 'when user is not signed in' do
-      let(:user_signed_in?) { false }
-
-      it 'does not create a new vote for the project' do
-        post :create, params: params, format: :js
-        expect(project).to_not have_received(:upvote_for).with(user)
+      before do
+        allow(controller).to receive(:user_signed_in?).and_return(false)
+        allow(Project).to receive(:find).with(project_id).and_return(project)
       end
-    end
 
-    context 'when user tries to vote his own project' do
-      it 'returns bad request' do
-        allow(controller).to receive(:current_user).and_return(other_user)
+      it 'returns a status of unauthorized' do
         post :create, params: params, format: :js
-        expect(response).to have_http_status(:bad_request)
+        expect(response).to have_http_status(:unauthorized)
       end
     end
   end
 
   describe '#destroy' do
-    it 'removes the users vote for the project' do
-      delete :destroy, params: params, format: :js
-      expect(project).to have_received(:remove_vote_for).with(user)
+    context 'when user is signed in' do
+      before do
+        allow(controller).to receive(:user_signed_in?).and_return(true)
+        allow(Project).to receive(:find).with(project_id).and_return(project)
+        allow(controller).to receive(:current_user).and_return(user)
+        allow(project).to receive(:remove_vote_for).with(user)
+      end
+
+      it 'removes the users vote for the project' do
+        delete :destroy, params: params, format: :js
+        expect(project).to have_received(:remove_vote_for).with(user)
+      end
     end
 
     context 'when user is not signed in' do
-      let(:user_signed_in?) { false }
+      before do
+        allow(controller).to receive(:user_signed_in?).and_return(false)
+        allow(Project).to receive(:find).with(project_id).and_return(project)
+      end
 
-      it 'does not create a new vote for the project' do
+      it 'returns a status of unauthorized' do
         delete :destroy, params: params, format: :js
-        expect(project).to_not have_received(:remove_vote_for).with(user)
+        expect(response).to have_http_status(:unauthorized)
       end
     end
   end
