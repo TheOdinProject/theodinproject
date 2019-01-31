6a9bf38cd7a709038d74ed7933c70c4e5e34694c
Andy D <mindovermiles262@gmail.com>
2019-01-31 10:34:19 -0800

    add tracks to seeds file

---
+++ db/seeds/08_full_stack_track.rb
@@ -0,0 +1,34 @@
+# CREATE TRACKS
+# *********************************
+
+Rails.logger.info "\n\n***** STARTING TRACKS *****"
+
+track_position = 0
+
+create_or_update_track(
+  title: "Full Stack",
+  description: "The OG Odin",
+  position: track_position,
+  courses: [
+      Course.find_by(title: "Web Development 101"),
+      Course.find_by(title: "Ruby Programming"),
+      Course.find_by(title: "Databases"),
+      Course.find_by(title: "Ruby on Rails"),
+      Course.find_by(title: "HTML and CSS"),
+      Course.find_by(title: "Javascript"),
+#      Course.find_by(title: "Getting Hired"),
+    ]
+)
+
+track_position += 1
+create_or_update_track(
+  title: "Ruby on Rails",
+  description: "Chugga Chugga",
+  position: track_position,
+  courses: [
+      Course.find_by(title: "Ruby Programming"),
+      Course.find_by(title: "Databases"),
+      Course.find_by(title: "Ruby on Rails"),
+    ]
+)
+
