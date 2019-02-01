f41873c0958f490b3deb4f7fa22c98d5de37abfe
Andy D <mindovermiles262@gmail.com>
2019-01-31 16:17:21 -0800

    fixes migration when no Tracks yet created

--- db/migrate/20190131192911_add_track_id_to_user.rb
+++ db/migrate/20190131192911_add_track_id_to_user.rb
@@ -1,5 +1,12 @@
 class AddTrackIdToUser < ActiveRecord::Migration[5.0]
+  def track_id
+    if Track.find_by(title: "Full Stack")
+      Track.find_by(title: "Full Stack").id
+    else
+      1
+    end
+  end
   def change
-    add_column :users, :track_id, :integer, default: Track.find_by(title: "Full Stack").id
+    add_column :users, :track_id, :integer, default: track_id
   end
 end
