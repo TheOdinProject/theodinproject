bb7153136b8b7af75f61daf09ce6ebf095733467
Andy D <mindovermiles262@gmail>
2019-02-02 13:27:49 -0800

    update seeds for tracks

--- db/seeds/08_full_stack_track.rb
+++ db/seeds/08_full_stack_track.rb
@@ -7,8 +7,18 @@ if Rails.env == "development"
   Track.delete_all
 end
 
-Track.create!(
+create_or_update_track(
   title: "Full Stack",
-  description: "The OG Odin",
-  position: 1
+  description: "The OG Odin: RELOADED",
+  position: 1,
+  courses: [
+    Course.find_by(title: "Web Development 101"),
+    Course.find_by(title: "Ruby Programming"),
+    Course.find_by(title: "Databases"),
+    Course.find_by(title: "Ruby on Rails"),
+    Course.find_by(title: "HTML and CSS"),
+    Course.find_by(title: "Javascript"),
+    Course.find_by(title: "Getting Hired")
+  ]
 )
+
