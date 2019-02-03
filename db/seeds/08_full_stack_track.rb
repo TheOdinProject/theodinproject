57e8430a7b807ae9294e4ef3e3f6196a1d385bfe
Andy D <mindovermiles262@gmail>
2019-02-02 21:28:34 -0800

    small edits from GH code review

--- db/seeds/08_full_stack_track.rb
+++ db/seeds/08_full_stack_track.rb
@@ -3,22 +3,18 @@
 
 Rails.logger.info "\n\n***** STARTING TRACKS *****"
 
-if Rails.env == "development"
-  Track.delete_all
-end
-
 create_or_update_track(
   title: "Full Stack",
   description: "The OG Odin",
   position: 1,
-  courses: [
-    Course.find_by(title: "Web Development 101"),
-    Course.find_by(title: "Ruby Programming"),
-    Course.find_by(title: "Databases"),
-    Course.find_by(title: "Ruby on Rails"),
-    Course.find_by(title: "HTML and CSS"),
-    Course.find_by(title: "Javascript"),
-    Course.find_by(title: "Getting Hired")
+  track_courses: [
+    TrackCourse.create(track: Track.find_by(title: "Full Stack"), course: Course.find_by(title: "Web Development 101"), track_position: 1),
+    TrackCourse.create(track: Track.find_by(title: "Full Stack"), course: Course.find_by(title: "Ruby Programming"), track_position: 2),
+    TrackCourse.create(track: Track.find_by(title: "Full Stack"), course: Course.find_by(title: "Databases"), track_position: 3),
+    TrackCourse.create(track: Track.find_by(title: "Full Stack"), course: Course.find_by(title: "Ruby on Rails"), track_position: 4),
+    TrackCourse.create(track: Track.find_by(title: "Full Stack"), course: Course.find_by(title: "HTML and CSS"), track_position: 5),
+    TrackCourse.create(track: Track.find_by(title: "Full Stack"), course: Course.find_by(title: "Javascript"), track_position: 6),
+    TrackCourse.create(track: Track.find_by(title: "Full Stack"), course: Course.find_by(title: "Getting Hired"), track_position: 7)
   ]
 )
 
