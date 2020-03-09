91955588c3b942e1ca2c173310976aaa0f431e4c
rsuazo <rsuazo2@gmail.com>
2020-03-09 13:03:28 -0700

    added knight class

--- rsuazo/hello.rb
+++ rsuazo/hello.rb
@@ -2,3 +2,11 @@ class GameBoard
   def initialize
   end
 end
+
+class Knight
+  attr_accessor: position
+
+  def initialize(x_coordinate = 0, y_coordinate = 0)
+    @position = (x_coordinate, y_coordinate)
+  end
+end
