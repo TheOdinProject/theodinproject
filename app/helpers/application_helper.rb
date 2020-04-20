b4a2522e6accac01f9abce5fddf07ffd81b10ea9
kshubhranshu <shubhranshu14@gmail.com>
2020-04-20 11:50:41 +0530

    added wrapper to tile hyperlinks && changed titile_url to path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -154,43 +154,43 @@ module ApplicationHelper
         badge_image_url: 'badge-html-css.svg',
         badge_alt_text: 'html css badge',
         title: 'HTML + CSS',
-        title_url: '/courses/html5-and-css3'
+        path: '/courses/html5-and-css3'
       },
       {
         badge_image_url: 'badge-javascript.svg',
         badge_alt_text: 'javascript badge',
         title: 'JavaScript',
-        title_url: '/courses/javascript'
+        path: '/courses/javascript'
       },
       { badge_image_url: 'badge-git.svg',
         badge_alt_text: 'git badge',
         title: 'Git',
-        title_url: '/courses/web-development-101'
+        path: '/courses/web-development-101'
       },
       { badge_image_url: 'badge-database.svg',
         badge_alt_text: 'databases badge',
         title: 'Databases',
-        title_url: '/courses/databases'
+        path: '/courses/databases'
       },
       { badge_image_url: 'badge-ruby.svg',
         badge_alt_text: 'ruby badge',
         title: 'Ruby',
-        title_url: '/courses/ruby-progrmming'
+        path: '/courses/ruby-progrmming'
       },
       { badge_image_url: 'badge-ruby-on-rails.svg',
         badge_alt_text: 'ruby on rails badge',
         title: 'Ruby on Rails',
-        title_url: '/courses/ruby-on-rails'
+        path: '/courses/ruby-on-rails'
       },
       { badge_image_url: 'badge-nodejs.svg',
         badge_alt_text: 'nodejs badge',
         title: 'NodeJS',
-        title_url: '/courses/nodejs'
+        path: '/courses/nodejs'
       },
       { badge_image_url: 'badge-getting-hired.svg',
         badge_alt_text: 'getting hired badge',
         title: 'Getting Hired',
-        title_url: '/courses/getting-hired'
+        path: '/courses/getting-hired'
       }
     ]
   end
