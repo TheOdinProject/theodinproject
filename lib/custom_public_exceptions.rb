ebb36d9c0e2611b106f22591e487e02a52716508
Matt Mont <mattmont415@gmail.com>
2020-08-27 18:57:59 -0700

    Played with one file

--- lib/custom_public_exceptions.rb
+++ lib/custom_public_exceptions.rb
@@ -1,7 +1,7 @@
 class CustomPublicExceptions < ActionDispatch::PublicExceptions
   def call(env)
     status = env['PATH_INFO'][1..-1]
-
+#Coding to comitting!
     if status == '404'
       Rails.application.routes.call(env)
     else
