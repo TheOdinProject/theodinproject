e9e3f28b3b1d29ecc806d2cae5a9f9e00bef9a97
Kevin <kevinmulhern23@gmail.com>
2020-09-21 02:41:12 +0100

    Chore: Project Submission Tweaks
    This Commit:
    * Autofocus on the first input in each modal.
    * Do not allow the body to scroll when a modal is opened.
    * Display the submissions list to signed out users.
    * Display a prompt to sign in when signed out users try to flag a submission.

--- app/javascript/components/project-submissions/containers/project-submissions-container.js
+++ app/javascript/components/project-submissions/containers/project-submissions-container.js
@@ -89,6 +89,11 @@ const ProjectSubmissions = (props) => {
     return submissions.find(submission => submission.user_id === userId);
   }, [userId, submissions.length]);
 
+  const showAddSubmissionButton = () => {
+    console.log(!userId)
+    !userSubmission || !userId
+  }
+
   return (
     <div className="submissions">
       <div className="submissions__header">
@@ -110,11 +115,12 @@ const ProjectSubmissions = (props) => {
             submission={flaggedSubmission}
             onSubmit={handleFlag}
             onClose={toggleShowFlagModal}
+            userId={userId}
           />
         </Modal>
 
         <div>
-          {!userSubmission && (
+          {showAddSubmissionButton() && (
             <button className="submissions__add button button--primary" onClick={toggleShowCreateModal}>
               Add Solution
             </button>
