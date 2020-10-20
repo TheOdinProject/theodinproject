1c82abfd98f82352af71ca5915cb712761f445e6
Zach Baird <zdb1994@yahoo.com>
2020-10-20 09:10:35 -0400

    Added comment on path filter.

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -6,6 +6,8 @@ class PathsController < ApplicationController
   end
 
   def index
+    # The filter on here is temporary until the path is removed and the students
+    #  enrolled in it are migrated to another path.
     @paths = Path.all.filter { |path| path.title != 'Front End Only' }
   end
 
