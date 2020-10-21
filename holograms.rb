7b47ab2a4daf9f4a8a59a27f3fe966520a38dc03
Rumsauce <tylerdownie0@gmail.com>
2020-10-21 12:49:41 -0700

    add initialize

--- holograms.rb
+++ holograms.rb
@@ -1,1 +1,15 @@
 ## Adds Star Wars like holograms to site.
+
+    Class hologram
+
+        :attr_accessor :type, :gender
+
+        def initialize(type, gender)
+
+            @type = type
+
+            @gender = gender
+
+        end
+
+    end
