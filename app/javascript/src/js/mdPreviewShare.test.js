64e4c315195941abad4fd19379f830397449e4ef
Tatiana <tatianaw83@gmail.com>
2021-01-09 22:39:23 -0800

    add additional test cases

--- app/javascript/src/js/mdPreviewShare.test.js
+++ app/javascript/src/js/mdPreviewShare.test.js
@@ -5,12 +5,28 @@ describe('encodeContent', () => {
     const buffer = Buffer.from('This is a test string!', 'utf-8');
     expect(encodeContent('This is a test string!')).toBe(buffer.toString('base64'));
   });
+  it('content including numbers is correctly encoded to base64', () => {
+    const buffer = Buffer.from('This is a 2nd test string!', 'utf-8');
+    expect(encodeContent('This is a 2nd test string!')).toBe(buffer.toString('base64'));
+  });
+  it('content including special characters and new lines correctly encoded to base64', () => {
+    const buffer = Buffer.from('This is a test string! \n # Next Title', 'utf-8');
+    expect(encodeContent('This is a test string! \n # Next Title')).toBe(buffer.toString('base64'));
+  });
 });
 
 describe('decodeContent', () => {
   it('content is correctly decoded from base64', () => {
     const buffer = Buffer.from('This is a test string!', 'utf-8').toString('base64');
     expect(decodeContent(buffer)).toBe('This is a test string!');
+  });
+  it('content including numbers is correctly decoded from base64', () => {
+    const buffer = Buffer.from('This is a 2nd test string!', 'utf-8').toString('base64');
+    expect(decodeContent(buffer)).toBe('This is a 2nd test string!');
+  });
+  it('content including special characters and new lines correctly decoded from base64', () => {
+    const buffer = Buffer.from('This is a test string! \n # Next Title', 'utf-8').toString('base64');
+    expect(decodeContent(buffer)).toBe('This is a test string! \n # Next Title');
   });
 });
 
