aee34f8e69d1f4ec9fec93b942ead05abd608601
Leonard Soai-Van <leo.soaivan@gmail.com>
2022-03-26 11:03:09 -0400

    Extract styling for Toggle

--- app/javascript/components/project-submissions/components/toggle.jsx
+++ app/javascript/components/project-submissions/components/toggle.jsx
@@ -2,22 +2,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const toggleContainerClassNames = `
-  relative inline-flex flex-shrink-0
-  h-6 w-12 py-2px
-  border-2 border-transparent rounded-full
-  cursor-pointer
-  transition-colors ease-in-out duration-200
-  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-odin-green
-`;
-const toggleBodyClassNames = `
-  inline-block
-  h-5 w-5
-  pointer-events-none
-  rounded-full bg-white shadow
-  transform ring-0 transition ease-in-out duration-200
-`;
-
 /**
  * react-hook-form's register does not apply to buttons, which is the root element of Tailwind's
  * toggle. We must therefore pass it's "value" between form parent component and Toggle, thereby
@@ -31,14 +15,14 @@ const Toggle = ({ label, isToggled, onClick }) => (
         id="is_public"
         type="button"
         onClick={onClick}
-        className={`${toggleContainerClassNames} ${isToggled ? 'bg-odin-green' : 'bg-gray-200'}`}
+        className={`top-toggle-container ${isToggled ? 'bg-odin-green' : 'bg-gray-200'}`}
         role="switch"
         aria-checked={isToggled}
       >
         <span className="sr-only">{label}</span>
         <span
           aria-hidden="true"
-          className={`${toggleBodyClassNames} ${isToggled ? 'translate-x-7' : 'translate-x-0'}`}
+          className={`top-toggle-body ${isToggled ? 'translate-x-7' : 'translate-x-0'}`}
         />
       </button>
     </div>
