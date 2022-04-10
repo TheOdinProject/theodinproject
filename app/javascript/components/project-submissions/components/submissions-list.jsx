46edf5c52778ff37127ec3c86822d0cbc2e9864c
L. P. Soai-Van <leo.soaivan@gmail.com>
2022-04-09 23:47:25 -0400

    Convert blank-slate message

--- app/javascript/components/project-submissions/components/submissions-list.jsx
+++ app/javascript/components/project-submissions/components/submissions-list.jsx
@@ -51,8 +51,10 @@ const SubmissionsList = ({
               />
             ))}
           </FlipMove>
+        ) : (
+          <h2 className="text-center text-gray-600 font-bold pt-6 pr-0 pb-2 mb-0">No Submissions yet, be the first!</h2>
         )
-        : <h2 className="submissions__blank-slate">No Submissions yet, be the first!</h2>}
+      }
 
       { allSubmissionsPath
         && (
