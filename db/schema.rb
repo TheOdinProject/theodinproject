925ac947dfedbb3d480e865ba8acba2c3269fff0
Kevin Mulhern <kevinmulhern23@gmail.com>
2022-04-14 22:49:04 +0100

    Chore: Rename announcements expires column to expires_at
    Because:
    * Rails conventions.
    
    This commit:
    * Rename announcements expires column to expires_at
    * Add not null constraint to the same column
    * Refactors the unexpired_messages scope to use a date range

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_13_201217) do
+ActiveRecord::Schema.define(version: 2022_04_14_212627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
