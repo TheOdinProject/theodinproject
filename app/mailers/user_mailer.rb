5a156870efc989bea89787e0a69df9710d8ecc9d
Kevin Mulhern <kevinmulhern23@gmail.com>
2022-05-05 21:28:36 +0100

    Fix: Emails being Sent Via Sendgrid
    Because:
    * Send grid now requires a sender identity, we need to use an email inbox we can access for the from address.
    
    This commit:
    * Update default email from address to contact@theodinproject.com

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'The Odin Project <contact@theodinproject.com>'
+  default from: 'contact@theodinproject.com'
 
   def send_welcome_email_to(user)
     @user = user
