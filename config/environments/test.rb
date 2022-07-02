52cc5fcc22978e5ed08014dedaafebb88d3688d4
Dylan Williams <happyfrog578+github@gmail.com>
2022-07-02 19:02:25 +1000

    Fix linting / spec errors and readjust overly aggressive config reset

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,6 +38,15 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = {
+    host: ENV.fetch('HOST', 'localhost'),
+    port: ENV.fetch('HOST_PORT', 3000)
+  }
+  routes.default_url_options = {
+    host: ENV.fetch('HOST', 'localhost'),
+    port: ENV.fetch('HOST_PORT', 3000)
+  }
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
--- db/migrate/20220702084524_add_service_name_to_active_storage_blobs.active_storage.rb
+++ db/migrate/20220702084524_add_service_name_to_active_storage_blobs.active_storage.rb
@@ -1,3 +1,4 @@
+# rubocop:disable all
 # This migration comes from active_storage (originally 20190112182829)
 class AddServiceNameToActiveStorageBlobs < ActiveRecord::Migration[6.0]
   def up
