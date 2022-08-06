1c4f0ef7bc4bdf98f92191d8a32580bf6ba75c1f
Kevin <kevinmulhern23@gmail.com>
2022-08-06 13:06:49 +0100

    Feature: Restyle Flash Messages with Tailwind.
    Because:
    * We were using the default Bootstap styles for our flash messages.
    
    This commit:
    * Always display flash messages in the upper right in a fixed position so they can be seen if the user is at the bottom of the page.
    * Add a flash View Component to handle the different types of flash messages.
    * Add a flash Stimulus JS controller to handle dismissing flash messages.
    * Cleans up partials used for flash messages - we didn't need two of them.
    * Move navbar, announcements and flash partials out of a partial and into the application layout.
    * Remove bootstrap helpers we used for selecting the correct styles for our old flash messages.

---
+++ spec/components/alerts/flash_component_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Alerts::FlashComponent, type: :component do
+  context 'when the type is alert' do
+    it 'renders an alert flash' do
+      component = described_class.new(type: 'alert', message: 'Fire!')
+
+      render_inline(component)
+
+      expect(page).to have_css('.bg-red-100')
+      expect(page).to have_content('Fire!')
+    end
+  end
+
+  context 'when the type is notice' do
+    it 'renders a notice flash' do
+      component = described_class.new(type: 'notice', message: 'Success!')
+
+      render_inline(component)
+
+      expect(page).to have_css('.bg-green-100')
+      expect(page).to have_content('Success!')
+    end
+  end
+end
