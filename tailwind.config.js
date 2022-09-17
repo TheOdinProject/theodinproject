f9770f506f619c6816b6e526f1b00405a6b1fe89
Kevin <kevinmulhern23@gmail.com>
2022-09-17 07:31:06 +0100

    Chore: Enable Tailwind Form Plugin by Default
    Because:
    * We want the Tailwind form styles to be automatically applied to any new form fields in the future.
    
    This commit:
    * Enable the Tailwind form plugin by default in Tailwind config.
    * Remove the classes used to enable the Tailwind form plugin for individual fields.
    * Fix email footer cta dark mode styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -101,8 +101,6 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
-    require("@tailwindcss/forms")({
-      strategy: 'class', // only generate classes
-    }),
+    require("@tailwindcss/forms"),
   ],
 }
