10b76993125cefa683ab2f20e2a7bc028883bf87
Kevin <kevinmulhern23@gmail.com>
2022-10-02 16:41:50 +0100

    Chore: Upgrade to Tailwind 3
    Because:
    * To keep things up to date.
    
    This commit:
    * Bump tailwind to latest version
    * Bump tailwind typography to latest version
    * Add stylesheet for prism theme so we can customise it as the typography bump lowered the specificity of the prose class with pre elemenets.

--- app/javascript/components/lesson-preview/components/lesson-content-preview.jsx
+++ app/javascript/components/lesson-preview/components/lesson-content-preview.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const LessonContentInput = ({ content }) => (
-  <div className="lesson-content prose prose-lg prose-gold min-h-screen"><div dangerouslySetInnerHTML={{ __html: content }} /></div>
+  <div className="lesson-content prose prose-lg prose-slate prose-a:text-gold-600 min-h-screen max-w-prose mx-auto"><div dangerouslySetInnerHTML={{ __html: content }} /></div>
 );
 
 LessonContentInput.defaultProps = {
