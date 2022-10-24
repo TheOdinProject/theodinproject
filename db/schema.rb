8d3cadea67d7027275ab1d5a8d1338efa5c9d0cf
Juan Vasquez <juan@ombulabs.com>
2022-10-24 14:34:31 -0500

    Enable pgcrypto

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2022_10_21_183414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pgcrypto"
 
   create_table "active_admin_comments", id: :serial, force: :cascade do |t|
     t.string "namespace"
