7ed7fb92e9c81b92af7c61f40d3d8a9eb2f3312e
Juan Vasquez <juan@ombulabs.com>
2022-10-25 10:26:38 -0500

    Enable null constrain

--- db/migrate/20221021183414_create_lesson_previews.rb
+++ db/migrate/20221021183414_create_lesson_previews.rb
@@ -1,7 +1,7 @@
 class CreateLessonPreviews < ActiveRecord::Migration[6.1]
   def change
     create_table :lesson_previews, id: :uuid do |t|
-      t.text :content
+      t.text :content, null: false
       t.timestamps
     end
   end
