dbee9f00b1e70936c2a5f41073cd3c597e96da2a
Juan Vasquez <juan@ombulabs.com>
2022-10-26 22:56:36 -0500

    Fix Rails/ActionOrder
    ```
    app/controllers/paths_controller.rb:10:3:
    C: [Correctable] Rails/ActionOrder: Action index should appear before show.
    ```
    
    Related https://github.com/TheOdinProject/theodinproject/pull/3459

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -1,14 +1,14 @@
 class PathsController < ApplicationController
+  def index
+    @foundations = Path.default_path
+    @fullstack_paths = Path.fullstack_paths
+  end
+
   def show
     @path = Path.friendly.find(params[:id])
 
     if @path.courses.size == 1
       redirect_to path_course_path(@path, @path.courses.first)
     end
-  end
-
-  def index
-    @foundations = Path.default_path
-    @fullstack_paths = Path.fullstack_paths
   end
 end
