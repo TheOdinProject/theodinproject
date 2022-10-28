68be7e920654d230e6f7f72d925f0733f784bd97
Juan Vasquez <juan@ombulabs.com>
2022-10-28 18:28:53 -0500

    Add length test

--- spec/models/lesson_preview_spec.rb
+++ spec/models/lesson_preview_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe LessonPreview, type: :model do
   it { is_expected.to validate_presence_of(:content) }
+  it { is_expected.to validate_length_of(:content).is_at_most(70_000) }
 end
