a56d1ef199901bf2917d1aedcc8ecefdb6adc6a3
Juan Vasquez <juan@ombulabs.com>
2022-10-28 21:14:05 -0500

    Run prettier

--- app/javascript/components/lesson-preview/components/share-button.jsx
+++ app/javascript/components/lesson-preview/components/share-button.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from '../../../src/js/axiosWithCsrf';
 
-const ShareButton = ({content}) => {
+const ShareButton = ({ content }) => {
   const [copied, setCopied] = useState(false);
 
   useEffect(() => {
@@ -12,24 +12,28 @@ const ShareButton = ({content}) => {
   }, [copied]);
 
   const handleOnClick = async () => {
-    const response = await axios.post('/lessons/preview', {content});
+    const response = await axios.post('/lessons/preview', { content });
     const previewLink = response.data.preview_link;
     navigator.clipboard.writeText(previewLink).then(() => setCopied(true));
   };
 
   return (
-    <button className={`button ${copied ? 'button--secondary' : 'button--primary'}`} onClick={handleOnClick} type="button">
+    <button
+      className={`button ${copied ? 'button--secondary' : 'button--primary'}`}
+      onClick={handleOnClick}
+      type="button"
+    >
       {copied ? 'Copied!' : 'Share'}
-    </button >
-  )
-}
+    </button>
+  );
+};
 
 ShareButton.defaultProps = {
-  content: ''
+  content: '',
 };
 
 ShareButton.propTypes = {
-  content: PropTypes.string
+  content: PropTypes.string,
 };
 
 export default ShareButton;
