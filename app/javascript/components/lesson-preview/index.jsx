9e7b948bdad48a8a9b51a4cb014240cb5a30539d
Juan Vasquez <juan@ombulabs.com>
2022-10-28 20:24:37 -0500

    Run pretter

--- app/javascript/components/lesson-preview/index.jsx
+++ app/javascript/components/lesson-preview/index.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import {
-  Tab, Tabs, TabList, TabPanel,
-} from 'react-tabs';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import Prism from 'prismjs';
 
 import LessonContentInput from './components/lesson-content-input';
@@ -10,7 +8,7 @@ import LessonContentPreview from './components/lesson-content-preview';
 import ShareButton from './components/share-button';
 import axios from '../../src/js/axiosWithCsrf';
 
-const LessonPreview = ({previewContent}) => {
+const LessonPreview = ({ previewContent }) => {
   const [content, setContent] = useState('');
   const [convertedContent, setConvertedContent] = useState('');
   const [onPreviewTab, setOnPreviewTab] = useState(false);
@@ -39,8 +37,18 @@ const LessonPreview = ({previewContent}) => {
   return (
     <Tabs selectedTabClassName="text-gray-700 bg-gray-300/50 hover:bg-gray-300 dark:bg-gray-700/90 dark:text-gray-300">
       <TabList className="flex items-center mb-3">
-        <Tab onClick={() => setOnPreviewTab(false)} className="text-gray-600 hover:text-gray-800 bg-gray-50 hover:bg-gray-200 dark:bg-gray-700/40 dark:text-gray-300 rounded-md border border-transparent px-3 py-1.5 font-medium cursor-pointer focus:outline-none">Write</Tab>
-        <Tab onClick={fetchLessonPreview} className="ml-2 text-gray-600 hover:text-gray-800 bg-gray-50 hover:bg-gray-200 dark:bg-gray-700/40 dark:text-gray-300 rounded-md border border-transparent px-3 py-1.5 font-medium cursor-pointer focus:outline-none">Preview</Tab>
+        <Tab
+          onClick={() => setOnPreviewTab(false)}
+          className="text-gray-600 hover:text-gray-800 bg-gray-50 hover:bg-gray-200 dark:bg-gray-700/40 dark:text-gray-300 rounded-md border border-transparent px-3 py-1.5 font-medium cursor-pointer focus:outline-none"
+        >
+          Write
+        </Tab>
+        <Tab
+          onClick={fetchLessonPreview}
+          className="ml-2 text-gray-600 hover:text-gray-800 bg-gray-50 hover:bg-gray-200 dark:bg-gray-700/40 dark:text-gray-300 rounded-md border border-transparent px-3 py-1.5 font-medium cursor-pointer focus:outline-none"
+        >
+          Preview
+        </Tab>
       </TabList>
 
       <TabPanel>
@@ -49,7 +57,7 @@ const LessonPreview = ({previewContent}) => {
       <TabPanel>
         <LessonContentPreview content={convertedContent} />
       </TabPanel>
-      <div className='flex pt-10 justify-end'>
+      <div className="flex pt-10 justify-end">
         {hasContent && <ShareButton content={content} />}
       </div>
     </Tabs>
@@ -57,11 +65,11 @@ const LessonPreview = ({previewContent}) => {
 };
 
 LessonPreview.defaultProps = {
-  previewContent: ''
+  previewContent: '',
 };
 
 LessonPreview.propTypes = {
-  previewContent: PropTypes.string
+  previewContent: PropTypes.string,
 };
 
 export default LessonPreview;
