dfd7a0825ef91d230c1d61dd190c91ba893f4bda
Juan Vasquez <juan@ombulabs.com>
2022-10-30 19:32:08 -0600

    Add maxLength

--- app/javascript/components/lesson-preview/components/lesson-content-input.jsx
+++ app/javascript/components/lesson-preview/components/lesson-content-input.jsx
@@ -8,10 +8,11 @@ const LessonContentInput = ({ onChange, content }) => {
 
   return (
     <textarea
-      className="w-full min-h-screen block rounded-md border-gray-300 dark:bg-gray-700/60 dark:focus:ring-2 dark:placeholder-gray-400 dark:border-gray-500 dark:focus:ring-gray-500 shadow-sm focus:border-blue-500 focus:ring-blue-500 dark-form-input"
-      placeholder="Lesson content..."
-      onChange={handleOnChange}
       autoFocus
+      className="w-full min-h-screen block rounded-md border-gray-300 dark:bg-gray-700/60 dark:focus:ring-2 dark:placeholder-gray-400 dark:border-gray-500 dark:focus:ring-gray-500 shadow-sm focus:border-blue-500 focus:ring-blue-500 dark-form-input"
+      maxLength={10}
+      onChange={handleOnChange}
+      placeholder="Lesson content..."
       value={content}
     />
   );
