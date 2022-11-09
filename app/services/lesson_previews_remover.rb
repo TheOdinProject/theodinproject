8ecca45ea9cb1f36282271c0ebda1c4e60a4a893
Juan Vasquez <juan@ombulabs.com>
2022-11-09 08:10:33 -0600

    Add expired scope

--- app/services/lesson_previews_remover.rb
+++ app/services/lesson_previews_remover.rb
@@ -1,12 +1,11 @@
 class LessonPreviewsRemover
   def call
-    more_than_one_month_lesson_previews_old.destroy_all
+    expired_lesson_previews.destroy_all
   end
 
   private
 
-  def more_than_one_month_lesson_previews_old
-    @more_than_one_month_lesson_previews_old ||=
-      LessonPreview.where(created_at: ..1.month.ago.end_of_day)
+  def expired_lesson_previews
+    @expired_lesson_previews ||= LessonPreview.expired
   end
 end
--- spec/models/lesson_preview_spec.rb
+++ spec/models/lesson_preview_spec.rb
@@ -3,4 +3,13 @@ require 'rails_helper'
 RSpec.describe LessonPreview, type: :model do
   it { is_expected.to validate_presence_of(:content) }
   it { is_expected.to validate_length_of(:content).is_at_most(70_000) }
+
+  describe ".expired" do
+    it "returns 2 expired lesson previews" do
+      expired = create_list(:lesson_preview, 2, created_at: 1.month.ago)
+      create_list(:lesson_preview, 2)
+
+      expect(LessonPreview.expired).to eq expired
+    end
+  end
 end
