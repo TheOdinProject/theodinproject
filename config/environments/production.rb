baee7418672b0f45edf4c562ff3961df46dc5430
Kevin <kevinmulhern23@gmail.com>
2023-01-04 09:12:24 +0000

    Fix: Resolve failing linters from bump

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   config.active_support.disallowed_deprecation_warnings = []
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  config.log_formatter = Logger::Formatter.new
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
