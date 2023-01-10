5c62af564e09d48d875674d4b30c0c25da476b37
Scheals <95084627+scheals@users.noreply.github.com>
2023-01-10 19:17:35 +0100

    tempfix: update styling for lesson preview

--- app/javascript/components/lesson-preview/components/lesson-content-preview.jsx
+++ app/javascript/components/lesson-preview/components/lesson-content-preview.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const LessonContentInput = ({ content }) => (
-  <div className="lesson-content max-w-none mx-auto xl:mx-0 prose prose-lg prose-gray prose-a:text-gold-600 dark:prose-code:bg-gray-700/60 dark:prose-pre:prose-code break-words line-numbers dark:prose-invert dark:antialiased min-h-screen">
+  <div className="lesson-content max-w-prose mx-auto prose md:prose-lg prose-gray prose-a:text-gold-600 prose-code:bg-gray-100 prose-code:p-1 prose-code:font-normal dark:prose-code:bg-gray-700/40 prose-code:rounded-md break-words line-numbers dark:prose-invert dark:antialiased prose-pre:rounded-xl prose-pre:bg-slate-800 prose-pre:shadow-lg dark:prose-pre:bg-slate-800/70 dark:prose-pre:shadow-none dark:prose-pre:ring-1 dark:prose-pre:ring-slate-300/10">
     <div dangerouslySetInnerHTML={{ __html: content }} />
   </div>
 );
