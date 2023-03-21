f7e7e328260e6bfb8c93df0bef93aca6b25d0610
Leo Kirasić <kirasicleo@gmail.com>
2023-03-21 14:26:50 +0100

    Add discord route

--- config/routes/redirects.rb
+++ config/routes/redirects.rb
@@ -2,4 +2,5 @@ require_relative 'redirectors/nested_lesson_redirector'
 Rails.application.routes.draw do
   get '/courses/web-development-101(/lessons/:id)', to: redirect('/paths/foundations')
   get '/paths/:path_id/courses/:course_id/lessons/:id', to: redirect(Redirectors::NestedLessonRedirector.new)
+  get '/discord', to: redirect('https://discord.gg/theodinproject')
 end
