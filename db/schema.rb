44295da4616eaa51ce67b9b9cff359cb7cebacf7
Cody Loyd <codyloyd@gmail.com>
2023-07-31 09:50:51 -0500

    Feature: Add OC information to site (#4038)
    ## Because
    We need to get this information out there.
    
    ## This PR
    adds the aforementioned information to the site.
    
    ## Additional Information
    do not merge until we're ready for the announcement.
    
    do not merge until we get the discord role stuff figured out
    
    ## Pull Request Requirements
    <!-- Replace the whitespace between the square brackets with an 'x',
    e.g. [x]. After you create the PR, they will become checkboxes that you
    can click on. -->
    - [x] I have thoroughly read and understand [The Odin Project
    Contributing
    Guide](https://github.com/TheOdinProject/theodinproject/blob/main/CONTRIBUTING.md)
    - [x] The title of this PR follows the `keyword: brief description of
    change` format, using one of the following keywords:
      - `Feature` - adds new or amends existing user-facing behavior
    - `Chore` - changes that have no user-facing value, refactors,
    dependency bumps, etc
      - `Fix` - bug fixes
    -   [x] The `Because` section summarizes the reason for this PR
    - [x] The `This PR` section has a bullet point list describing the
    changes in this PR
    - [ ] I have verified all tests and linters pass after making these
    changes.
    - [ ] If this PR addresses an open issue, it is linked in the `Issue`
    section
    -   [ ] If applicable, this PR includes new or updated automated tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_28_230910) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_28_225009) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
