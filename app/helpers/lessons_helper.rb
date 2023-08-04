92fe7cf7321613862039795866413df8bb03b2a1
JustWaveThings <justwavethings@proton.me>
2023-08-03 23:09:51 -0400

    Add github report url method

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -2,4 +2,8 @@ module LessonsHelper
   def github_edit_url(lesson)
     github_link("curriculum/edit/main#{lesson.github_path}")
   end
+  def github_report_url(lesson)
+    github_link("curriculum/issues/new?assignees=nil&labels=Status%3A+Needs+Triage&projects=&template=suggestion.yaml&title=#{lesson.title}%3A+%3CShort+description+of+your+suggestion%3E")
+  end
 end
+
