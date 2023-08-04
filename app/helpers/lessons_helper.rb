f4be298ed0f41901eb5b256a1eca293fb62ee01f
JustWaveThings <justwavethings@proton.me>
2023-08-04 00:02:54 -0400

    Linting

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -2,8 +2,10 @@ module LessonsHelper
   def github_edit_url(lesson)
     github_link("curriculum/edit/main#{lesson.github_path}")
   end
+
   def github_report_url(lesson)
+    # rubocop:disable Layout/LineLength
     github_link("curriculum/issues/new?assignees=nil&labels=Status%3A+Needs+Triage&projects=&template=suggestion.yaml&title=#{lesson.title}%3A+%3CShort+description+of+your+suggestion%3E")
+    # rubocop:enable Layout/LineLength
   end
 end
-
