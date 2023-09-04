c1ab56eb45cfb725a680d1fff0bba58aa079e4b2
Asartea <76259120+Asartea@users.noreply.github.com>
2023-09-04 16:41:21 +0200

    Add redirect from /blog to the TOP blog

--- config/routes/redirects.rb
+++ config/routes/redirects.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/courses/web-development-101(/lessons/:id)', to: redirect('/paths/foundations')
   get '/paths/:path_id/courses/:course_id/lessons/:id', to: redirect(Redirectors::NestedLessonRedirector.new)
   get '/discord', to: redirect(ODIN_CHAT_URL)
+  get '/blog', to: redirect(ODIN_BLOG_URL)
 end
