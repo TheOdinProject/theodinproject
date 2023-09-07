e13fad8fc34d6550a89f0a7b0b713875e76c5cde
Asartea <76259120+Asartea@users.noreply.github.com>
2023-09-07 20:19:23 +0200

    Chore: Cleanup Discord links

--- config/routes/redirects.rb
+++ config/routes/redirects.rb
@@ -2,6 +2,6 @@ require_relative 'redirectors/nested_lesson_redirector'
 Rails.application.routes.draw do
   get '/courses/web-development-101(/lessons/:id)', to: redirect('/paths/foundations')
   get '/paths/:path_id/courses/:course_id/lessons/:id', to: redirect(Redirectors::NestedLessonRedirector.new)
-  get '/discord', to: redirect(ODIN_CHAT_URL)
+  get '/discord', to: redirect(ODIN_CHAT_URL), as: :discord
   get '/blog', to: redirect(ODIN_BLOG_URL), as: :blog
 end
--- spec/helpers/button_helper_spec.rb
+++ spec/helpers/button_helper_spec.rb
@@ -27,9 +27,7 @@ RSpec.describe ButtonHelper do
 
   describe '#chat_button' do
     let(:chat_button) do
-      # rubocop:disable Layout/LineLength
-      '<a class="button button--secondary px-4" target="_blank" rel="noreferrer" href="https://discord.gg/fbFCkYabZB">Open Discord</a>'
-      # rubocop:enable Layout/LineLength
+      '<a class="button button--secondary px-4" target="_blank" rel="noreferrer" href="/discord">Open Discord</a>'
     end
 
     it 'returns a chat button' do
