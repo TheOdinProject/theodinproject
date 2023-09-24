a40da1ac841ff23073102f26a16b21b66b3be4a6
Kevin <kevinmulhern23@gmail.com>
2023-09-24 13:46:29 +0100

    Chore: Remove view component data attribute helper
    Because:
    * We can simplify and use Rails 7's [`tag.attributes` helper](https://blog.saeloun.com/2021/05/05/rails-7-transform-hash-into-html-for-erb-interpolation/) instead
    
    This commit:
    * Removes `html_data_attributes_for` view component helper method.
    * Removes data attributes parameter from card components - it wasn't used anywhere.
    * Adds a couple of spec for testing cards with ids and custom classes

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,12 +1,4 @@
 class ApplicationComponent < ViewComponent::Base
   include Classy::Yaml::ComponentHelpers
   include Turbo::FramesHelper
-
-  private
-
-  def html_data_attributes_for(data)
-    data.map do |key, value|
-      "data-#{key.to_s.dasherize}=#{value.to_s.dasherize}"
-    end.join(' ')
-  end
 end
