1148297d38da7c5487b78209f61bbd2f9730cc96
Asartea <76259120+Asartea@users.noreply.github.com>
2023-10-29 21:14:51 +0100

    fix properly this time

--- spec/helpers/lessons_helper_spec.rb
+++ spec/helpers/lessons_helper_spec.rb
@@ -12,9 +12,11 @@ RSpec.describe LessonsHelper do
   end
 
   describe '#github_report_url' do
-    let(:lesson) { create(:lesson, title: 'Ruby Basics') }
+    let(:course) { create(:course, title: 'Test Course1') }
+    let(:lesson) { create(:lesson, title: 'Ruby Basics', course:) }
 
     it 'returns the correct github url' do
+      puts(helper.github_report_url(lesson))
       expect(helper.github_report_url(lesson)).to eql(
         'https://github.com/TheOdinProject/curriculum/issues/new?labels=Status%3A+Needs+Triage&lesson-link=http%3A%2F%2Ftest.host%2Flessons%2Ftest-course1-ruby-basics&template=suggestion.yaml&title=Ruby+Basics%3A+%3CShort+description+of+your+suggestion%3E'
       )
