1cf12eb583ef376e76a24ca8fd12a954ac8457e2
Kevin <kevinmulhern23@gmail.com>
2023-10-29 14:59:54 +0000

    Chore: Remove Axios
    Because:
    * We no longer use it anywhere in the codebase. Any remote fetching is done with Rails/Request.js

--- app/javascript/src/js/axiosWithCsrf.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const token = document.querySelector('meta[name=csrf-token]') || { content: 'no-csrf-token' };
-
-const axiosInstance = axios.create();
-axiosInstance.defaults.headers.common['X-CSRF-Token'] = token.content;
-
-export default axiosInstance;
