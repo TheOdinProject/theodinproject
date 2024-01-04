6e2b94a9f7a200c8c68e7331047e888afbcb053b
“sparshalc” <imailsparsha@gmail.com>
2024-01-04 15:20:40 +0545

    Fix broken home page banner

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,18 +3,10 @@ class UserMailer < ApplicationMailer
 
   def send_welcome_email_to(user)
     @user = user
-    attachments.inline['logo.svg'] = logo_path
-
     mail(
       subject: 'Getting started with The Odin Project',
       to: user.email,
       template_name: 'welcome_email',
     )
-  end
-
-  private
-
-  def logo_path
-    Rails.root.join('app/assets/images/logo.svg').read
   end
 end
