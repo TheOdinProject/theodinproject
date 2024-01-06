9cfc9212aa2c2ecef1e84f44334214d9a47f3c2c
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-06 14:31:37 +0100

    Add ruby lesson

--- db/fixtures/lessons/ruby_lessons.rb
+++ db/fixtures/lessons/ruby_lessons.rb
@@ -318,5 +318,21 @@ def ruby_lessons
       previewable: false,
       identifier_uuid: 'cea1b51e-b97e-11eb-8529-0242ac130003',
     },
+    'HashMap' => {
+      title: 'HashMap',
+      description: 'Learn how a hash map works, to save and retrieve data',
+      is_project: false,
+      github_path: '/ruby/computer_science/hash_map_data_structure.md',
+      identifier_uuid: 'create-uuid'
+    },
+    'HashMap Project' => {
+      title: 'HashMap Project',
+      description: 'Build your very first hash map data structure from scratch',
+      is_project: true,
+      github_path: '/ruby/computer_science/project_hash_map.md',
+      accepts_submission: true,
+      previewable: false,
+      identifier_uuid: 'create-uuid'
+    },
   }
 end
