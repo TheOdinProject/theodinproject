e6afb50ce2200b9fbd89f9f7842aaaa2f689b57d
“sparshalc” <imailsparsha@gmail.com>
2024-01-13 10:36:56 +0545

    Add feature to display the number of projects in each section

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,4 +33,14 @@ module ApplicationHelper
   def unread_notifications?(user)
     user.notifications.any?(&:unread?)
   end
+
+  def project_count_for_course(course)
+    true_count = course.lessons.count(&:is_project)
+    content_tag(:span, "#{true_count} Projects", class: 'text-sm text-gray-500')
+  end
+
+  def lesson_count_for_course(course)
+    true_count = course.lessons.count { |lesson| !lesson.is_project }
+    content_tag(:span, "#{true_count} Lessons", class: 'text-sm text-gray-500')
+  end
 end
