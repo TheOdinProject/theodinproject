b2052b4b09c1a7c8b12a00cdcbbb5f04ef5c22c7
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-18 20:13:31 +0100

    Refactor into smaller methods

--- app/services/search_indexer.rb
+++ app/services/search_indexer.rb
@@ -3,24 +3,15 @@ require 'nokogiri'
 class SearchIndexer
   def self.index_frequencies
     total_word_count = Hash.new(0)
-    lesson_word_count = {}
+    lessons_word_count = {}
 
     Lesson.find_each do |lesson|
       text = extract_lesson_document(lesson)
       tokens = tokenize(text, total_word_count)
-      lesson_word_count[lesson.id] = tokens
+      lessons_word_count[lesson.id] = tokens
     end
 
-    word_frequencies = []
-    progressbar = ProgressBar.create total: Lesson.count, format: '%t: |%w%i| 2/2 Completed: %c %a %e'
-    lesson_word_count.each do |lesson_id, word_count|
-      word_count.each do |word, tf|
-        tf_idf = (tf.to_f / word_count.length.to_f) * Math.log((1 + Lesson.count.to_f) / (1 + total_word_count[word].to_f))
-        word_frequencies << { lesson_id:, word:, tf_idf: }
-      end
-      progressbar.increment
-    end
-
+    word_frequencies = extract_word_frequencies(lessons_word_count, total_word_count, Lesson.count)
     WordFrequency.insert_all(word_frequencies)
   end
 
@@ -42,6 +33,21 @@ class SearchIndexer
     # doc = Nokogiri::HTML5.parse(lesson.body)
     # doc.css('code').remove
     # text = ((lesson.title + ' ') * 5) + doc.text
-    text = lesson.title
+    lesson.title
+  end
+
+  def self.extract_word_frequencies(lessons_word_count, total_word_count, total_documents)
+    word_frequencies = []
+    progressbar = ProgressBar.create total: total_documents, format: '%t: |%w%i| 2/2 Completed: %c %a %e'
+    lessons_word_count.each do |lesson_id, word_count|
+      word_count.each do |word, tf|
+        tf_idf = (tf.to_f / word_count.length.to_f) * Math.log((1 + total_documents.to_f) / (1 + total_word_count[word].to_f))
+        word_frequencies << { lesson_id:, word:, tf_idf: }
+      end
+
+      progressbar.increment
+    end
+
+    word_frequencies
   end
 end
