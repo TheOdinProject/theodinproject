b71df2a519fb74701e0c9224379eac80d2357a63
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-18 09:45:33 +0100

    Add search api

---
+++ app/controllers/api/search_controller.rb
@@ -0,0 +1,25 @@
+class Api::SearchController < ApplicationController
+  # skip_before_action :verify_authenticity_token
+  # before_action :authenticate
+
+  def index
+    query = params[:query].scan(/\b\w+\b/)
+    lesson_ids = WordFrequency
+                 .where(word: query)
+                 .group(:lesson_id)
+                 .select('lesson_id, SUM(tf_idf) as total_tf_idf')
+                 .order('total_tf_idf DESC')
+                 .limit(10)
+                 .map(&:lesson_id)
+    lessons = Lesson.where(id: lesson_ids).index_by(&:id).values_at(*lesson_ids)
+    render json: lessons
+  end
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_token do |token|
+      ActiveSupport::SecurityUtils.secure_compare(token, ENV['ODIN_BOT_ACCESS_TOKEN'])
+    end
+  end
+end
