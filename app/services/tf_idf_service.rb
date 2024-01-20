cf93a04a9abaa089c392dec71d99c0c3d9f89949
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-20 20:27:35 +0100

    Update tf_idf_service to exclude stop_words

--- app/services/tf_idf_service.rb
+++ app/services/tf_idf_service.rb
@@ -1,24 +1,38 @@
 class TfIdfService
   def initialize
-    @df_table = Hash.new(0)
+    @stop_words = @df_table = Hash.new(0)
     @tf_table = {}
     @total_documents = 0
   end
 
+  def self.stop_words
+    Set.new(%w[
+              a an and are as at be by for from has he in is it its of on that the to was were will with I you your
+              yours him his she her hers they them their theirs we us our ours this these those who whom whose what
+              which where when why how am being been do does did have had having me my mine myself yourself yourselves
+              himself herself itself themselves ourselves but or nor so yet up down over under above below between among
+              through out off about against during here there all any both each few more most other some such no not
+              same another own certain first last next many much good great new big man one two
+              three four five six seven eight nine ten
+            ])
+  end
+
   def populate_table(key, title, text)
-    @total_documents += 1
-    tokenize(key, title, text)
+    unless @tf_table.key?(key) && @tf_table[key].key?(title)
+      @total_documents += 1
+      tokenize(key, title, text)
+    end
   end
 
   def tokenize(key, title, text)
     tf_map = Hash.new(0)
     words = text.scan(/\b\w+\b/)
     words.each do |word|
+      next if @stop_words.include?(word)
+
       word = word.downcase
       tf_map[word] += 1
-      if tf_map[word] == 1
-        @df_table[word] += 1
-      end
+      @df_table[word] += 1 if tf_map[word] == 1
     end
 
     @tf_table[key] = { title:, tf_map: }
