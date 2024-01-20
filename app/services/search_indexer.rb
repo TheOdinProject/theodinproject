4c84efff328263a687816b5c24159ca035970f41
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-20 17:25:17 +0100

    Fix bug returning title duplicated

--- app/services/search_indexer.rb
+++ app/services/search_indexer.rb
@@ -2,16 +2,21 @@ require 'nokogiri'
 
 class SearchIndexer
   def self.index_frequencies
-    total_word_count = Hash.new(0)
-    lessons_word_count = {}
+    # Lesson.find_each do |lesson|
+    #   text = extract_lesson_document(lesson)
+    #   tokens = tokenize(text, total_word_count)
+    #   lessons_word_count[lesson.id] = tokens
+    # end
 
-    Lesson.find_each do |lesson|
-      text = extract_lesson_document(lesson)
-      tokens = tokenize(text, total_word_count)
-      lessons_word_count[lesson.id] = tokens
+    tf_idf = TfIdfService.new
+
+    title = 'this is a title'
+    text = "this is a very long text supposedly so let's see if it actually works"
+    url = 'key.com'
+    tf_idf.populate_table(url, title, text)
+
+    tf_idf.tf_idf_list.each do |list|
+      puts list.inspect
     end
-
-    tf_idf_listtract_word_frequencies(lessons_word_count, total_word_count, Lesson.count)
-    WordFrequency.insert_all(tf_idf_list)
   end
 end
--- app/services/tf_idf_service.rb
+++ app/services/tf_idf_service.rb
@@ -1,39 +1,39 @@
 class TfIdfService
   def initialize
-    @df_table = {}
+    @df_table = Hash.new(0)
     @tf_table = {}
     @total_documents = 0
   end
 
-  def populate_table(key, text, title)
+  def populate_table(key, title, text)
     @total_documents += 1
     tokenize(key, title, text)
   end
 
   def tokenize(key, title, text)
-    word_count = Hash.new(0)
+    tf_map = Hash.new(0)
     words = text.scan(/\b\w+\b/)
     words.each do |word|
       word = word.downcase
-      word_count[word] += 1
-      if word_count[word] == 1
+      tf_map[word] += 1
+      if tf_map[word] == 1
         @df_table[word] += 1
       end
     end
 
-    @tf_table << { key:, title:, word_count: }
+    @tf_table[key] = { title:, tf_map: }
   end
 
   def tf_idf_list
     tf_idf_list = []
-    @tf_table.each do |key, tf_map|
+    @tf_table.each do |key, record|
       tf_idf = []
-      tf_map.each_key do |word|
+      record[:tf_map].each_key do |word|
         tf_idf_score = calculate_tf_idf_score(key, word)
         tf_idf << [word:, tf_idf_score:]
       end
 
-      @tf_idf_list << { key:, tf_idf: }
+      tf_idf_list << { key:, title: record[:title], tf_idf: }
     end
     tf_idf_list
   end
@@ -42,7 +42,7 @@ class TfIdfService
     tf_table = @tf_table[key]
 
     total_tf = tf_table.length
-    tf = tf_table[word]
+    tf = tf_table[:tf_map][word]
     df = @df_table[word]
     (tf.to_f / total_tf) * Math.log((1 + @total_documents.to_f) / (1 + df))
   end
