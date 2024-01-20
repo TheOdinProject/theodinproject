9266e4730f8f7b9b556d288e3a800b8af42790b0
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-20 23:43:44 +0100

    Add unique index for tf_idf table

--- db/migrate/20240120161214_create_tf_idfs.rb
+++ db/migrate/20240120161214_create_tf_idfs.rb
@@ -5,5 +5,7 @@ class CreateTfIdfs < ActiveRecord::Migration[7.0]
       t.float :tf_idf, null: false
       t.belongs_to :search_record, foreign_key: true, null: false
     end
+
+    add_index :tf_idfs, %i[search_record_id word], unique: true
   end
 end
