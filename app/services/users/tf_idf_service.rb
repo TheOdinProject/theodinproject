fb2c15518a34d0476b19b6ba84be73ca5c8df2bc
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-20 17:05:48 +0100

    Finalize tf_idf service

--- app/services/users/tf_idf_service.rb
+++ app/services/users/tf_idf_service.rb
@@ -3,15 +3,14 @@ class TfIdfService
     @df_table = {}
     @tf_table = {}
     @total_documents = 0
-    @tf_idf_list = []
   end
 
   def populate_table(key, text, title)
     @total_documents += 1
-    tokenize(key, text + title)
+    tokenize(key, title, text)
   end
 
-  def tokenize(key, text)
+  def tokenize(key, title, text)
     word_count = Hash.new(0)
     words = text.scan(/\b\w+\b/)
     words.each do |word|
@@ -22,19 +21,24 @@ class TfIdfService
       end
     end
 
-    @tf_table << { key:, word_count: }
+    @tf_table << { key:, title:, word_count: }
   end
 
-  def generate_tf_idf_list
-    @tf_table.each do |_key, tf_map|
-      tf_map.each do |word, _tf|
-        tf_idf = calculate_tf_idf(key, word)
-        tf_idf_list << { lesson_id:, word:, tf_idf: }
+  def tf_idf_list
+    tf_idf_list = []
+    @tf_table.each do |key, tf_map|
+      tf_idf = []
+      tf_map.each_key do |word|
+        tf_idf_score = calculate_tf_idf_score(key, word)
+        tf_idf << [word:, tf_idf_score:]
       end
+
+      @tf_idf_list << { key:, tf_idf: }
     end
+    tf_idf_list
   end
 
-  def calculate_tf_idf(key, word)
+  def calculate_tf_idf_score(key, word)
     tf_table = @tf_table[key]
 
     total_tf = tf_table.length
