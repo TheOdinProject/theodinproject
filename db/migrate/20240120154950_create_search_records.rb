4a4927e114665d875702a8c73f13f192fe61b627
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-20 15:57:59 +0100

    Add unique restrictions

--- db/migrate/20240120154950_create_search_records.rb
+++ db/migrate/20240120154950_create_search_records.rb
@@ -1,8 +1,8 @@
 class SearchRecords < ActiveRecord::Migration[6.0]
   def change
     create_table :search_records do |t|
-      t.string :title, null: false
-      t.string :url, null: false
+      t.string :title, null: false, unique: true
+      t.string :url, null: false, unique: true
     end
   end
 end
