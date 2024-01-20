d515c4938ab7ffbafd7b4d41e159bf017bc14bbf
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-20 19:49:36 +0100

    Refactor service to extract indexes for new table schema

--- app/services/search_indexer.rb
+++ app/services/search_indexer.rb
@@ -2,21 +2,22 @@ require 'nokogiri'
 
 class SearchIndexer
   def self.index_frequencies
-    # Lesson.find_each do |lesson|
-    #   text = extract_lesson_document(lesson)
-    #   tokens = tokenize(text, total_word_count)
-    #   lessons_word_count[lesson.id] = tokens
-    # end
-
     tf_idf = TfIdfService.new
-
-    title = 'this is a title'
-    text = "this is a very long text supposedly so let's see if it actually works"
-    url = 'key.com'
-    tf_idf.populate_table(url, title, text)
-
-    tf_idf.tf_idf_list.each do |list|
-      puts list.inspect
+    Lesson.find_each do |lesson|
+      record = parse_lesson(lesson)
+      tf_idf.populate_table(record[:url], record[:title], record[:text])
     end
+
+    count = 0
+    tf_idf.tf_idf_list.each do |list|
+      count += list[:tf_idf].length
+    end
+
+    puts count
+  end
+
+  def self.parse_lesson(lesson)
+    text = Nokogiri::HTML5.parse(lesson.body).text
+    { url: "https://www.theodinproject.com/lessons/#{lesson.slug}", title: lesson.title, text: }
   end
 end
