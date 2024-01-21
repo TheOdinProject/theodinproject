da086b8128b37d215b6ffdcb52214aefcbcfc976
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-21 06:32:45 +0100

    Refactor controller

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -3,26 +3,14 @@ class Api::SearchController < ApplicationController
   before_action :authenticate
 
   def index
-    query = params[:query].scan(/\b\w+\b/)
-    lesson_ids = WordFrequency
-                 .where(word: query)
-                 .group(:lesson_id)
-                 .select('lesson_id, SUM(tf_idf) as total_tf_idf')
-                 .order('total_tf_idf DESC')
-                 .map(&:lesson_id)
-
-    uuid = Set.new
-    lessons_hash = {}
-    lessons = Lesson
-              .where(id: lesson_ids)
-              .each do |lesson|
-      unless uuid.include?(lesson.title)
-        lessons_hash[lesson.id] = { url: 'https://www.theodinproject.com/lessons/' + lesson.slug, title: lesson.title, desc: lesson.description }
-        uuid.add(lesson.title)
-      end
-    end
-
-    render json: lesson_ids.map { |id| lessons_hash[id] }.compact
+    query = params[:q].scan(/\b\w+\b/)
+    render json: TfIdf
+      .where(word: query)
+      .group(:lesson_id)
+      .select('lesson_id, SUM(score) as total_score')
+      .order('total_score DESC')
+      .map(&:lesson_id)
+      .flat_map { |id| Lesson.where(id:) }
   end
 
   private
