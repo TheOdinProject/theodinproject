359329379e21382bb9b1741d48b6d4ac409c0a61
Kevin <kevinmulhern23@gmail.com>
2024-03-03 11:56:47 +0000

    Feature: Re-enable auto-sorting when like value changes. (#4420)
    Because:
    - Previously, we would auto sort the solutions by their likes count when
    a like value changed.
    - We removed it because it was thought to be redundant with manual
    sorting now available.
    - A staff member mentioned missing it, so its back!
    
    This commit:
    - Only auto-sort if the current manual sort value is by like count - we
    don't want this kicking in when users want to see lists ordered by
    newest or oldest.
    - Adds an item controller for solutions which will handle updating the
    sort code when a like value changes.
    - Reinstate the removed auto sort controller which will now observe for
    any mutations to the sort codes of each item.

---
+++ app/components/project_submissions/item_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static targets = ['likeCount'];
+
+  static values = {
+    likeCount: String,
+  };
+
+  likeCountTargetConnected(element) {
+    const currentLikeCount = element.innerText;
+
+    if (currentLikeCount !== this.likeCountValue) {
+      this.likeCountValue = currentLikeCount;
+    }
+  }
+
+  likeCountValueChanged(value) {
+    const sortCode = this.element.dataset.sortCode
+
+    if (sortCode != value) {
+      this.element.dataset.sortCode = value;
+    }
+  }
+}
