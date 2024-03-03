7c1f784b0121f161ef315533246f54cad4665fe4
Kevin <kevinmulhern23@gmail.com>
2024-03-03 16:45:20 +0000

    Feature: Add Shell for Admin V2
    Because:
    - Admin v2 needs to be easy to navigate and have plenty of room to grow in the future.
    
    This commit:
    - Adds a sidebar plus top nav-bar navigation areas
    - Adds placeholder nav items for flags and announcements
    - Allows admins to sign out

--- app/controllers/admin_v2/sessions_controller.rb
+++ app/controllers/admin_v2/sessions_controller.rb
@@ -1,4 +1,9 @@
 module AdminV2
   class SessionsController < Devise::SessionsController
+    private
+
+    def after_sign_out_path_for(_resource_or_scope)
+      new_admin_user_session_path
+    end
   end
 end
