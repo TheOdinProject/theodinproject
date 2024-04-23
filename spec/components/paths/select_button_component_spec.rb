2c367195d9fd4657051f48dd35e77eec51e03651
David Hernandez <d.hdza19@gmail.com>
2024-04-23 00:14:07 -0500

    feat: add Continue button to selected path

--- spec/components/paths/select_button_component_spec.rb
+++ spec/components/paths/select_button_component_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Paths::SelectButtonComponent, type: :component do
   context 'when leaner is on the path' do
-    it 'renders the resume button' do
+    it 'renders the continue button' do
       path = create(:path)
       current_user = create(:user, path:)
       component = described_class.new(current_user:, path:)
 
       render_inline(component)
 
-      expect(page).to have_button('Resume')
+      expect(page).to have_button('Continue')
     end
   end
 
