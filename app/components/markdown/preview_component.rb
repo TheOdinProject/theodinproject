1d1b69fd3e4009b238e45b02ad75b77c72125bde
Zach <zdb1994@yahoo.com>
2024-05-08 15:28:47 -0400

    Feature: Adds realtime linting for markdown previewer.

--- app/components/markdown/preview_component.rb
+++ app/components/markdown/preview_component.rb
@@ -1,6 +1,20 @@
 class Markdown::PreviewComponent < ApplicationComponent
   def initialize(markdown: '')
     @markdown = markdown
+    @lint_errors = []
+  end
+
+  def lint
+    File.write('markdown_to_lint.md', @markdown)
+    results = `markdownlint-cli2 markdown_to_lint.md 2>&1`
+    File.delete('markdown_to_lint.md')
+
+    @lint_errors = results.split("\n").drop(4)
+    @lint_errors.each_with_index do |err, i|
+      @lint_errors[i] = err.split.drop(2).join(' ')
+    end
+
+    @lint_errors
   end
 
   def allowed_tags
