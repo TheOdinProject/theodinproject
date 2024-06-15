6c49967f412760651ecd928066dcf15782b6653b
Kevin <kevinmulhern23@gmail.com>
2024-06-15 17:39:56 +0100

    Fix: Phantom timeout flash notice.
    Because
    - This is a bug with devise - https://github.com/heartcombo/devise/issues/1777
    - When you are timed out, a flash will appear with "true" as the message
    
    This commit
    - Do not render the flash component if the type is "timedout"

--- app/components/alerts/flash_component.rb
+++ app/components/alerts/flash_component.rb
@@ -1,14 +1,19 @@
 class Alerts::FlashComponent < ApplicationComponent
+  DISSALLOWED_TYPES = %i[timedout].freeze
+
   def initialize(type:, message:)
     @type = type.to_sym
     @message = message
+  end
+
+  def render?
+    DISSALLOWED_TYPES.exclude?(type)
   end
 
   def icon_path
     {
       alert: 'icons/exclamation-circle-solid.svg',
       notice: 'icons/checkmark-circle-solid.svg',
-      timedout: 'icons/exclamation-circle-solid.svg'
     }.fetch(type)
   end
 
