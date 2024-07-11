6747bd2675ed5cf79b9a9075f4a459dc9ffcaaad
Kevin <kevinmulhern23@gmail.com>
2024-07-11 09:58:41 +0100

    Chore: Tune Rate limiting (#4610)
    Rate limit... 
    - sign ups
    - sign ins
    - password resets
    - Overall rate limit for every page when request count for an ip goes
    over a certain threshold.
    
    ## Pull Request Requirements
    <!-- Replace the whitespace between the square brackets with an 'x',
    e.g. [x]. After you create the PR, they will become checkboxes that you
    can click on. -->
    - [x] I have thoroughly read and understand [The Odin Project
    Contributing
    Guide](https://github.com/TheOdinProject/theodinproject/blob/main/CONTRIBUTING.md)
    - [x] The title of this PR follows the `keyword: brief description of
    change` format, using one of the following keywords:
        - `Feature` - adds new or amends existing user-facing behavior
    - `Chore` - changes that have no user-facing value, refactors,
    dependency bumps, etc
        - `Fix` - bug fixes
    -   [x] The `Because` section summarizes the reason for this PR
    - [x] The `This PR` section has a bullet point list describing the
    changes in this PR
    - [x] I have verified all tests and linters pass after making these
    changes.
    - [x] If this PR addresses an open issue, it is linked in the `Issue`
    section
    -   [x] If applicable, this PR includes new or updated automated tests

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,5 +1,61 @@
 class Rack::Attack
-  Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+  ### Throttle Spammy Clients ###
+
+  # If any single client IP is making tons of requests, then they're
+  # probably malicious or a poorly-configured scraper. Either way, they
+  # don't deserve to hog all of the app server's CPU. Cut them off!
+  #
+  # Note: If you're serving assets through rack, those requests may be
+  # counted by rack-attack and this throttle may be activated too
+  # quickly. If so, enable the condition to exclude them from tracking.
+
+  # Throttle all requests by IP (60rpm)
+  #
+  # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
+  throttle('req/ip', limit: 300, period: 5.minutes, &:ip)
+
+  ### Prevent Brute-Force Login Attacks ###
+
+  # The most common brute-force login attack is a brute-force password
+  # attack where an attacker simply tries a large number of emails and
+  # passwords to see if any credentials match.
+  #
+  # Another common method of attack is to use a swarm of computers with
+  # different IPs to try brute-forcing a password for a specific account.
+
+  # Throttle POST requests to /users/sign_in by IP address
+  #
+  # Key: "rack::attack:#{Time.now.to_i/:period}:logins/ip:#{req.ip}"
+  throttle('logins/ip', limit: 5, period: 20.seconds) do |req|
+    req.ip if req.path == '/users/sign_in' && req.post?
+  end
+
+  # Throttle POST requests to /login by email param
+  #
+  # Key: "rack::attack:#{Time.now.to_i/:period}:logins/email:#{normalized_email}"
+  throttle('logins/email', limit: 5, period: 20.seconds) do |req|
+    if req.path == '/users/sign_in' && req.post?
+      # Normalize the email, using the same logic as your authentication process, to
+      # protect against rate limit bypasses. Return the normalized email if present, nil otherwise.
+      req.params['email'].to_s.downcase.gsub(/\s+/, '').presence
+    end
+  end
+
+  # Throttle POST requests to /sign_up by IP address
+  #
+  # Key: "rack::attack:#{Time.now.to_i/:period}:sign_ups/ip:#{req.ip}"
+  throttle('sign_ups/ip', limit: 5, period: 20.seconds) do |req|
+    req.ip if req.path == '/users' && req.post?
+  end
+
+  # Throttle PATCH requests to /users/password_reset by IP address
+  #
+  # Key: "rack::attack:#{Time.now.to_i/:period}:password_resets/ip:#{req.ip}"
+  throttle('password_resets/ip', limit: 5, period: 20.seconds) do |req|
+    req.ip if req.path == '/users/password_reset' && req.post?
+  end
+
+  ### Custom Throttling
 
   Rack::Attack.throttle('report_ip', limit: 3, period: 60) do |request|
     request.ip if request.path.ends_with?('/flags') && request.post?
