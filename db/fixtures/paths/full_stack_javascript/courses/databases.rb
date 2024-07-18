b10604c7d2558316d8f798d57199205f6ab8fe16
01zulfi <85733202+01zulfi@users.noreply.github.com>
2024-07-18 11:38:57 +0500

    feat: node revamp milestone 1

---
+++ db/fixtures/paths/full_stack_javascript/courses/databases.rb
@@ -0,0 +1,28 @@
+#######################
+# Course - Databases
+#######################
+course = @path.add_course do |course|
+  course.title = 'Databases'
+  course.description = 'Databases are used to organize and capture large amounts of data, typically by inputting, storing, retrieving and managing the information. This course will focus on relational databases, which are widely used to store data and SQL, the language used to query the database.'
+  course.identifier_uuid = '8179728b-8401-4653-9839-5843c99cad7d'
+  course.show_on_homepage = true
+  course.badge_uri = 'badge-database.svg'
+end
+
+# +++++++++++++++++++
+# SECTION - Databases
+# +++++++++++++++++++
+course.add_section do |section|
+  section.title = 'Databases'
+  section.description = 'ORMs in Node.js do a lot of the heavy lifting with connecting and querying a database but there will be times you need to tweak a query to the database using raw SQL. Learning how to query efficiently will help your understanding of what Rails helps abstract away.'
+  section.identifier_uuid = '81f416b2-a0d0-4363-b9c1-341de3a685c1'
+
+  section.add_lessons(
+    database_lessons.fetch('Databases'),
+    database_lessons.fetch('Databases and SQL'),
+    database_lessons.fetch('SQL Zoo'),
+  )
+end
+
+# clean up any removed seeds from the database
+course.delete_removed_seeds
