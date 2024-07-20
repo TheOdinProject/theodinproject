55c42a499fc436eca670f4c9d8169db7c78c6db4
Asartea <76259120+Asartea@users.noreply.github.com>
2024-07-20 18:45:28 +0200

    Feat: Rewrite Databases section description (#4626)
    <!-- Thank you for taking the time to contribute to The Odin Project. In
    order to get this pull request (PR) merged in a reasonable amount of
    time, you must complete this entire template. -->
    
    ## Because
    - This is the NodeJS version, so it shouldn't mention Rails
    - Mentioning ORMs at this stage is weird, since the learner won't have
    been introduced to them yet
    
    ## This PR
    - Rewrites the section description to be more generic
    
    ## Pull Request Requirements
    <!-- Replace the whitespace between the square brackets with an 'x',
    e.g. [x]. After you create the PR, they will become checkboxes that you
    can click on. -->
    - [x] I have thoroughly read and understand [The Odin Project
    Contributing
    Guide](https://github.com/TheOdinProject/theodinproject/blob/main/CONTRIBUTING.md)
    - [x] The title of this PR follows the `keyword: brief description of
    change` format, using one of the following keywords:
        - `Feature` - adds new or amends existing user-facing behavior
    - `Chore` - changes that have no user-facing value, refactors,
    dependency bumps, etc
        - `Fix` - bug fixes
    -   [x] The `Because` section summarizes the reason for this PR
    - [x] The `This PR` section has a bullet point list describing the
    changes in this PR
    - [x] I have verified all tests and linters pass after making these
    changes.

--- db/fixtures/paths/full_stack_javascript/courses/databases.rb
+++ db/fixtures/paths/full_stack_javascript/courses/databases.rb
@@ -13,7 +13,7 @@ end
 # +++++++++++++++++++
 course.add_section do |section|
   section.title = 'Databases'
-  section.description = 'ORMs in Node.js do a lot of the heavy lifting with connecting and querying a database but there will be times you need to tweak a query to the database using raw SQL. Learning how to query efficiently will help your understanding of what Rails helps abstract away.'
+  section.description = 'When working with databases, there may be situations where you need to directly interact with the database using raw SQL queries. Learning SQL will enable you to effectively manipulate and retrieve data from the database.'
   section.identifier_uuid = '81f416b2-a0d0-4363-b9c1-341de3a685c1'
 
   section.add_lessons(
