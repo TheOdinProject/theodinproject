ea35ba027fa73b13a278ad040901ecc0791917f0
Kevin <kevinmulhern23@gmail.com>
2024-07-20 17:45:11 +0100

    Feature: Admin v2 - Resend Invite (#4628)
    Because:
    - When a pending team member did not receive their invite or cannot
    accept the initial invite, I want to send it again, so they can join the
    team.
    - Closes: https://github.com/TheOdinProject/theodinproject/issues/4627
    
    This commit:
    - Adds a "Resend invite" option to pending team member drop downs.

---
+++ app/controllers/admin_v2/team_members/resend_invitation_controller.rb
@@ -0,0 +1,22 @@
+module AdminV2
+  class TeamMembers::ResendInvitationController < AdminV2::BaseController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+    def create
+      team_member = AdminUser.find(params[:team_member_id])
+
+      if team_member.pending?
+        team_member.invite!(current_admin_user)
+        redirect_to admin_v2_team_path, notice: "Invite sent to #{team_member.name}"
+      else
+        redirect_to admin_v2_team_path, alert: "#{team_member.name} has already accepted the invitation"
+      end
+    end
+
+    private
+
+    def record_not_found
+      redirect_to admin_v2_team_path, alert: 'Team member not found'
+    end
+  end
+end
