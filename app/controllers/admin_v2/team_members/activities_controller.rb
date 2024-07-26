d6b0a275c26c4a6fdffdc57f15f0bafd097b1bfe
Kevin <kevinmulhern23@gmail.com>
2024-07-26 13:54:29 +0100

    Chore: Move manage team page to team members controller (#4652)
    Because:
    - A separate team route, controller and view directory are redundant
    when we can have use an index action on the team members controller.

--- app/controllers/admin_v2/team_members/activities_controller.rb
+++ app/controllers/admin_v2/team_members/activities_controller.rb
@@ -1,5 +1,5 @@
 module AdminV2
-  class Team::ActivitiesController < AdminV2::BaseController
+  class TeamMembers::ActivitiesController < AdminV2::BaseController
     def index
       @activities = PublicActivity::Activity.where(trackable_type: 'AdminUser').order(created_at: :desc)
     end
