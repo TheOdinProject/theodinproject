78499cfda15fd5cd616a46edb88aa21b6a2693bf
Kevin <kevinmulhern23@gmail.com>
2024-07-27 12:24:01 +0100

    Feature: Admin V2 - Create activity when password reset email is sent (#4662)
    Because:
    - When I reset an admin team members password, I want an activity to be
    created, so it is auditable.
    - Closes: https://github.com/TheOdinProject/theodinproject/issues/4661

--- app/controllers/admin_v2/team_members/password_resets_controller.rb
+++ app/controllers/admin_v2/team_members/password_resets_controller.rb
@@ -6,6 +6,7 @@ module AdminV2
       team_member = AdminUser.find(params[:team_member_id])
 
       team_member.send_reset_password_instructions
+      team_member.create_activity(key: 'admin_user.password_reset', owner: current_admin_user)
       redirect_to admin_v2_team_path, notice: 'Reset password instructions sent'
     end
 
