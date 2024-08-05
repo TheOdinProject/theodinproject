998b98ebb8c33cac9b4a3046fc439a0eda1f271a
Charu <charumishra5673@gmail.com>
2024-08-05 18:58:58 +0530

    Feat: Removed active_admin_comments table

--- db/schema.rb
+++ db/schema.rb
@@ -19,20 +19,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_02_085933) do
   create_enum "admin_roles", ["moderator", "maintainer", "core"]
   create_enum "admin_user_status", ["pending", "activated", "deactivated", "pending_reactivation"]
 
-  create_table "active_admin_comments", id: :serial, force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.integer "resource_id"
-    t.string "author_type"
-    t.integer "author_id"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
-
   create_table "activities", force: :cascade do |t|
     t.string "trackable_type"
     t.bigint "trackable_id"
