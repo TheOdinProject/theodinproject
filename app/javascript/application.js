a0af480d6b870a84ddfef35ac8cdcf27a0dbb63e
Kevin <kevinmulhern23@gmail.com>
2024-08-08 08:28:57 +0100

    Chore: Replace hint.css with a stimulus js tippy.js wrapper. (#4723)
    Because:
    - It gives us more control over tool tips
    - It means we won't need to load in a second stylesheet generated by the
    JS bundler
    
    This commit:
    - Removes hint.css
    - Adds tippy.js
    - Adds a Stimulus JS tooltip controller

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -16,7 +16,6 @@
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import 'hint.css/hint.min.css';
 import 'controllers';
 import '@hotwired/turbo-rails';
 import './src/custom_turbo_stream_actions';
