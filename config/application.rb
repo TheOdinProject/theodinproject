29b0cfe2f67fbe83a04c65f43671ab70b83c17b9
Dylan Will <58506115+ChargrilledChook@users.noreply.github.com>
2024-08-10 14:11:04 +1000

    Add generators to new `autoload_lib` configuration
    Because: CI breaks otherwise

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Theodinproject
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks]) # TODO
+    config.autoload_lib(ignore: %w[assets tasks generators]) # TODO
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
