4ef51829bf0775695cb158e913cd09a538f32f7d
Kevin <kevinmulhern23@gmail.com>
2024-08-11 08:59:14 +0100

    Chore: Add Sentry source maps
    Because:
    - It will help us diagnose JavaScript exceptions

--- app/javascript/src/sentry.js
+++ app/javascript/src/sentry.js
@@ -8,8 +8,7 @@ window.Sentry = window.Sentry || {}
 window.Sentry && Sentry.onLoad && Sentry.onLoad(() => {
   Sentry.init({
     dsn: SENTRY_DSN,
-    environment: 'production',
-    debug: true
+    environment: 'production'
   })
 
   if (currentUserSignedIn) {
