a166bc49f4101ac41a33c3ea5e489bae1f62d0ff
Austin Sullivan <a.m.sullivan.github@gmail.com>
2024-08-11 10:17:18 -0400

    Update contact email

--- config/initializers/odin_configuration.rb
+++ config/initializers/odin_configuration.rb
@@ -1,3 +1,3 @@
 ODIN_BLOG_URL = 'https://dev.to/theodinproject'.freeze
 ODIN_CHAT_URL = 'https://discord.gg/fbFCkYabZB'.freeze
-CONTACT_EMAIL = 'theodinprojectcontact@gmail.com'.freeze
+CONTACT_EMAIL = 'contact@theodinproject.com'.freeze
