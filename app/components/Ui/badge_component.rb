4d5260feafc6bbdafb69a9a0fd30f33b8ef36bac
Kevin <kevinmulhern23@gmail.com>
2024-08-18 09:54:11 +0100

    Feature: Optimise dark mode styling for badges
    Because:
    - Badges didn't look very good on dark mode, they were too bright :high_brightness:

--- app/components/Ui/badge_component.rb
+++ app/components/Ui/badge_component.rb
@@ -6,11 +6,12 @@ class Ui::BadgeComponent < ApplicationComponent
   def color_classes
     case color
     when 'yellow'
-      'bg-yellow-50 text-yellow-700 ring-yellow-600/20'
+      'bg-yellow-50 text-yellow-700 ring-yellow-600/20 ' \
+      'dark:bg-yellow-400/10 dark:text-yellow-500 dark:ring-yellow-400/20'
     when 'green'
-      'bg-green-50 text-green-700 ring-green-600/20'
+      'bg-green-50 text-green-700 ring-green-600/20 dark:bg-green-500/10 dark:text-green-400 dark:ring-green-500/20'
     else
-      'bg-gray-50 text-gray-700 ring-gray-600/20'
+      'bg-gray-50 text-gray-700 ring-gray-600/20 dark:bg-gray-400/10 dark:text-gray-400 dark:ring-gray-400/20'
     end
   end
 
