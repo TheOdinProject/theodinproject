5231a1bedd0c02ec9f47ba2a31d454486222f0ab
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-09-03 18:05:41 +0100

    Move diagramming controller connection from content container to lesson show view
    Was originally in the content container component to be shared between
    the lesson view and markdown preview tool, but the markdown preview tool
    needs a different connection/action structure to render mermaid diagrams
    properly.
    
    Moving it out of the content container means the diagramming#connect method
    can be essentially returned to original using the default .mermaid selector.

--- app/javascript/controllers/diagramming_controller.js
+++ app/javascript/controllers/diagramming_controller.js
@@ -3,10 +3,7 @@ import mermaid from 'mermaid'
 
 export default class DiagrammingController extends Controller {
   async connect () {
-    const isMarkdownPreview = Boolean(document.querySelector('#preview-container'))
-    const querySelector = isMarkdownPreview ? ':not(.hidden) > #preview-container .mermaid' : '.mermaid'
-
     mermaid.initialize({ startOnLoad: false, theme: 'dark' })
-    await mermaid.run({ querySelector })
+    await mermaid.run()
   }
 }
