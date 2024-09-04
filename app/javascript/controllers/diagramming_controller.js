8fe8dd816430715c2df3febf3148993b8b9a45ca
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-09-04 13:15:57 +0100

    Extract diagram rendering to own method

--- app/javascript/controllers/diagramming_controller.js
+++ app/javascript/controllers/diagramming_controller.js
@@ -2,7 +2,11 @@ import { Controller } from '@hotwired/stimulus'
 import mermaid from 'mermaid'
 
 export default class DiagrammingController extends Controller {
-  async connect () {
+  connect () {
+    this.render()
+  }
+
+  async render () {
     mermaid.initialize({ startOnLoad: false, theme: 'dark' })
     await mermaid.run()
   }
