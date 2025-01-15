97a3e13fccf4feb7696147d4c490f81ce10b2cd4
Kevin <kevinmulhern23@gmail.com>
2025-01-15 17:37:14 +0000

    Chore: Remove explicit protect from forgery callback (#4887)
    Because:
    - By default it already enabled and included in
    `ActionController::Base`. Our declaration in `ApplicationController` is
    unnecessary.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   include CurrentTheme
   include Pagy::Backend
 
-  protect_from_forgery with: :exception
-
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_sentry_user, if: :current_user
   before_action :store_user_location!, if: :storable_location?
