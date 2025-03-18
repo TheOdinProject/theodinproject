36bf8fd65f9cfaff815d295ef3090e08ee485a44
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2025-03-18 17:30:33 -0500

    updated changes from pull request

--- db/migrate/20250215224143_create_interview_surveys.rb
+++ db/migrate/20250215224143_create_interview_surveys.rb
@@ -2,7 +2,7 @@ class CreateInterviewSurveys < ActiveRecord::Migration[7.1]
   def change
     create_table :interview_surveys do |t|
       t.references :user, null: false, foreign_key: true
-      t.date :interview_date
+      t.date :interview_date, null: false
 
       t.timestamps
     end
