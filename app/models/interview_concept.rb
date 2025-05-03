4cff548e6a77fde0704ceaf42f0fb06329e58ce6
Josh Smith <jmsmith1018@gmail.com>
2025-05-03 13:06:01 -0600

    Add inverse association for interview_concepts

--- app/models/interview_concept.rb
+++ app/models/interview_concept.rb
@@ -1,5 +1,6 @@
 class InterviewConcept < ApplicationRecord
   has_many :interview_survey_concepts, dependent: :destroy, inverse_of: :interview_concept
+  has_many :interview_surveys, through: :interview_survey_concepts, inverse_of: :interview_concepts
 
   validates :name, presence: true
   validates :name, length: { minimum: 2, maximum: 25 }
