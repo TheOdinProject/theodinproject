b72d57028fa201c0600a835dcb69f9912a37c593
Josh Smith <jmsmith1018@gmail.com>
2025-05-03 12:42:37 -0600

    Add `inverse_of` option to association

--- app/models/interview_concept.rb
+++ app/models/interview_concept.rb
@@ -1,5 +1,5 @@
 class InterviewConcept < ApplicationRecord
-  has_many :interview_survey_concepts, dependent: :destroy
+  has_many :interview_survey_concepts, dependent: :destroy, inverse_of: :interview_concept
 
   validates :name, presence: true
   validates :name, length: { minimum: 2, maximum: 25 }
