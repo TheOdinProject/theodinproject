82273691dd2644c261604347b18fa3c9649160ef
Josh Smith <jmsmith1018@gmail.com>
2025-05-03 12:43:15 -0600

    Add form for filling out interview surveys

--- app/controllers/interview_surveys_controller.rb
+++ app/controllers/interview_surveys_controller.rb
@@ -1,14 +1,36 @@
 class InterviewSurveysController < ApplicationController
+  before_action :authenticate_user!
+  before_action :feature_flag_redirect
+
   def new
     Flipper.enable(:survey_feature) unless Rails.env.production?
 
-    unless Feature.enabled?(:survey_feature, current_user)
-      redirect_to dashboard_path, notice: 'Feature not enabled'
-    end
+    @interview_survey = InterviewSurvey.new
+    @interview_survey.interview_concepts.build
   end
 
   def create
-    # puts params[:survey]
-    redirect_to dashboard_path, notice: 'Survey Submitted'
+    @interview_survey = current_user.interview_surveys.build(interview_survey_params)
+
+    if @interview_survey.save!
+      redirect_to dashboard_path, notice: 'Survey Submitted'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def interview_survey_params
+    params.require(:interview_survey).permit(
+      :interview_date,
+      concept_list: []
+    )
+  end
+
+  def feature_flag_redirect
+    unless Feature.enabled?(:survey_feature, current_user)
+      redirect_to dashboard_path, notice: 'Feature not enabled'
+    end
   end
 end
--- app/models/interview_survey.rb
+++ app/models/interview_survey.rb
@@ -1,12 +1,20 @@
 class InterviewSurvey < ApplicationRecord
   belongs_to :user
-  has_many :interview_survey_concepts, dependent: :destroy
-  has_many :interview_concepts, through: :interview_survey_concepts
+  has_many :interview_survey_concepts, dependent: :destroy, inverse_of: :interview_survey
+  has_many :interview_concepts,
+           through: :interview_survey_concepts,
+           inverse_of: :interview_surveys
 
   accepts_nested_attributes_for :interview_concepts
 
   validates :interview_date, presence: true
   validate :interview_date_must_be_in_the_past
+
+  def concept_list=(concept_names)
+    self.interview_concepts = concept_names.compact_blank.map do |name|
+      InterviewConcept.find_or_create_by(name:)
+    end
+  end
 
   def interview_date_must_be_in_the_past
     return if interview_date.present? && interview_date <= Time.zone.today
