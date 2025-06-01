0bdea063704462c77c1fe7e84976f10f0a9b91d9
Josh Smith <jmsmith1018@gmail.com>
2025-06-01 10:26:30 -0600

    Satisfy JS and ERB linters

--- app/javascript/controllers/multi_select_controller.js
+++ app/javascript/controllers/multi_select_controller.js
@@ -2,11 +2,12 @@ import { Controller } from '@hotwired/stimulus'
 import SlimSelect from 'slim-select'
 
 export default class MultiSelectController extends Controller {
-  connect() {
+  connect () {
+    // eslint-disable-next-line no-new
     new SlimSelect({
       select: this.element,
       events: {
-        addable: function(value) { return value; }
+        addable: function (value) { return value }
       }
     })
   }
