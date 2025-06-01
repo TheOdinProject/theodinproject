40c75f830d45a7a7ec2cb81b9f47d2c15d33126a
Josh Smith <jmsmith1018@gmail.com>
2025-05-31 20:56:02 -0600

    Add system test for interview surveys with existing concepts

--- spec/system/interview_survey_spec.rb
+++ spec/system/interview_survey_spec.rb
@@ -10,11 +10,25 @@ RSpec.describe 'Interview survey' do
   context 'when the feature is enabled' do
     before do
       Flipper.enable(:survey_feature)
+      create(:interview_concept, name: 'Rails routing')
     end
 
     it 'shows the survey' do
       visit new_interview_survey_path
       expect(page).to have_content('Interview Survey')
+    end
+
+    it 'creates an interview survey with existing concepts' do
+      visit new_interview_survey_path
+
+      fill_in :interview_survey_interview_date, with: Date.current
+
+      find('div[aria-label="Combobox"]').click
+      find('div[class="ss-option"]', text: 'Rails routing').click
+
+      click_on 'Submit'
+
+      expect(page).to have_content('Survey Submitted')
     end
   end
 
