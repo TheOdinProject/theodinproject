a3edd4a06899af607e812326dc3a65c5519a2546
Dhruv <dhruvgupta766671@gmail.com>
2025-07-02 18:18:53 +0530

    Adding foundation docker, cloud, sqlite

---
+++ db/fixtures/lessons/foundation_cloud_computing.rb
@@ -0,0 +1,60 @@
+def foundation_lesson_cloud_computing
+  {
+  'How Internet Works' => {
+    title: 'How Internet Works',
+    description: 'How Internet Works',
+    is_project: false,
+    github_path: '/curriculum2/01_foundation/05_cloud_computing/00_how_internet_works.md',
+    identifier_uuid: 'create_uuid',
+  },
+  'Hosting on Local' => {
+    title: 'Hosting on Local',
+    description: 'Hosting on Local',
+    is_project: false,
+    github_path: '/curriculum2/01_foundation/05_cloud_computing/01_hosting_on_local.md',
+    identifier_uuid: 'create_uuid',
+  },
+  'Why Cloud Computing' => {
+    title: 'Why Cloud Computing',
+    description: 'Why Cloud Computing',
+    is_project: false,
+    github_path: '/curriculum2/01_foundation/05_cloud_computing/02_why_coud_computing.md',
+    identifier_uuid: 'create_uuid',
+  },
+  'Business of Cloud Computing' => {
+    title: 'Business of Cloud Computing',
+    description: 'Business of Cloud Computing',
+    is_project: false,
+    github_path: '/curriculum2/01_foundation/05_cloud_computing/03_business_of_cloud_computing.md',
+    identifier_uuid: 'create_uuid',
+  },
+  'Intro to AWS' => {
+    title: 'Intro to AWS',
+    description: 'Intro to AWS',
+    is_project: false,
+    github_path: '/curriculum2/01_foundation/05_cloud_computing/04_intro_to_aws.md',
+    identifier_uuid: 'create_uuid',
+  },
+  'S3' => {
+    title: 'S3',
+    description: 'S3',
+    is_project: false,
+    github_path: '/curriculum2/01_foundation/05_cloud_computing/05_S3.md',
+    identifier_uuid: 'create_uuid',
+  },
+  'EC2 Instances & EBS' => {
+    title: 'EC2 Instances & EBS',
+    description: 'EC2 Instances & EBS',
+    is_project: false,
+    github_path: '/curriculum2/01_foundation/05_cloud_computing/06_ec2_instances_ebs.md',
+    identifier_uuid: 'create_uuid',
+  },
+  'Billing and Cost Optimization' => {
+    title: 'Billing and Cost Optimization',
+    description: 'Billing and Cost Optimization',
+    is_project: false,
+    github_path: '/curriculum2/01_foundation/05_cloud_computing/07_billing_and_cost_optimization.md',
+    identifier_uuid: 'create_uuid',
+  },
+}
+end
---
+++ db/fixtures/lessons/foundation_docker.rb
@@ -0,0 +1,53 @@
+def foundation_lesson_docker
+  {
+  'It works on My Machine' => {
+    title: 'It works on My Machine',
+    description: 'It works on My Machine',
+    is_project: false,
+    github_path: '/curriculum2/01_foundation/04_docker/01_it_works_on_my_machine.md',
+    identifier_uuid: 'create_uuid',
+  },
+  'Runtime Isolation' => {
+    title: 'Runtime Isolation',
+    description: 'Runtime Isolation',
+    is_project: false,
+    github_path: '/curriculum2/01_foundation/04_docker/02_runtime_isolation.md',
+    identifier_uuid: 'create_uuid',
+  },
+  'Containerization' => {
+    title: 'Containerization',
+    description: 'Containerization',
+    is_project: false,
+    github_path: '/curriculum2/01_foundation/04_docker/03_containerization.md',
+    identifier_uuid: 'create_uuid',
+  },
+  'Getting Started with Docker' => {
+    title: 'Getting Started with Docker',
+    description: 'Getting Started with Docker',
+    is_project: false,
+    github_path: '/curriculum2/01_foundation/04_docker/04_getting_started_with_docker.md',
+    identifier_uuid: 'create_uuid',
+  },
+  'Optimizing Dockerfile' => {
+    title: 'Optimizing Dockerfile',
+    description: 'Optimizing Dockerfile',
+    is_project: false,
+    github_path: '/curriculum2/01_foundation/04_docker/05_optimizing_dockerfile.md',
+    identifier_uuid: 'create_uuid',
+  },
+  'Docker Compose' => {
+    title: 'Docker Compose',
+    description: 'Docker Compose',
+    is_project: false,
+    github_path: '/curriculum2/01_foundation/04_docker/06_docker_compose.md',
+    identifier_uuid: 'create_uuid',
+  },
+  'Knowledge Check' => {
+    title: 'Knowledge Check',
+    description: 'Knowledge Check',
+    is_project: false,
+    github_path: '/curriculum2/01_foundation/04_docker/07_knowledge_check.md',
+    identifier_uuid: 'create_uuid',
+  },
+}
+end
