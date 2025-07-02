fd6320754cf3c93ed70f22852a0ae32804dab8ea
Vivek <vivekjain.vj.2019@gmail.com>
2025-07-02 18:57:02 +0530

    adds python backend

--- db/fixtures/lessons/foundation_sqlite.rb
+++ db/fixtures/lessons/foundation_sqlite.rb
@@ -1,39 +1,39 @@
 def foundation_lesson_sqlite
   {
-  'Why Tabular and Relational' => {
-    title: 'Why Tabular and Relational',
-    description: 'Why Tabular and Relational',
-    is_project: false,
-    github_path: '/curriculum2/01_foundation/03_sqlite/01_why_tabular_and_relational.md',
-    identifier_uuid: 'create_uuid',
-  },
-  'SQLite in Everyday Life' => {
-    title: 'SQLite in Everyday Life',
-    description: 'SQLite in Everyday Life',
-    is_project: false,
-    github_path: '/curriculum2/01_foundation/03_sqlite/02_sqlite_in_everyday_life.md',
-    identifier_uuid: 'create_uuid',
-  },
-  'Learn SQL' => {
-    title: 'Learn SQL',
-    description: 'Learn SQL',
-    is_project: false,
-    github_path: '/curriculum2/01_foundation/03_sqlite/03_learn_sql.md',
-    identifier_uuid: 'create_uuid',
-  },
-  'Getting Started with SQLite' => {
-    title: 'Getting Started with SQLite',
-    description: 'Getting Started with SQLite',
-    is_project: false,
-    github_path: '/curriculum2/01_foundation/03_sqlite/04_getting_started_with_sqlite.md',
-    identifier_uuid: 'create_uuid',
-  },
-  'Exercises' => {
-    title: 'Exercises',
-    description: 'Exercises',
-    is_project: false,
-    github_path: '/curriculum2/01_foundation/03_sqlite/05_exercises.md',
-    identifier_uuid: 'create_uuid',
-  },
-}
+    'Why Tabular and Relational' => {
+      title: 'Why Tabular and Relational',
+      description: 'Why Tabular and Relational',
+      is_project: false,
+      github_path: '/curriculum2/01_foundation/03_sqlite/01_why_tabular_and_relational.md',
+      identifier_uuid: 'create_uuid',
+    },
+    'SQLite in Everyday Life' => {
+      title: 'SQLite in Everyday Life',
+      description: 'SQLite in Everyday Life',
+      is_project: false,
+      github_path: '/curriculum2/01_foundation/03_sqlite/02_sqlite_in_everyday_life.md',
+      identifier_uuid: 'create_uuid',
+    },
+    'Learn SQL' => {
+      title: 'Learn SQL',
+      description: 'Learn SQL',
+      is_project: false,
+      github_path: '/curriculum2/01_foundation/03_sqlite/03_learn_sql.md',
+      identifier_uuid: 'create_uuid',
+    },
+    'Getting Started with SQLite' => {
+      title: 'Getting Started with SQLite',
+      description: 'Getting Started with SQLite',
+      is_project: false,
+      github_path: '/curriculum2/01_foundation/03_sqlite/04_getting_started_with_sqlite.md',
+      identifier_uuid: 'create_uuid',
+    },
+    'Exercises' => {
+      title: 'Exercises',
+      description: 'Exercises',
+      is_project: false,
+      github_path: '/curriculum2/01_foundation/03_sqlite/05_exercises.md',
+      identifier_uuid: 'create_uuid',
+    },
+  }
 end
---
+++ db/fixtures/lessons/python_backend.rb
@@ -0,0 +1,115 @@
+  def python_backend_lessons
+    {
+      'Fast API kickoff' => {
+        title: 'Fast API kickoff',
+        description: 'Fast API kickoff',
+        is_project: false,
+        github_path: '/curriculum2/02_python_backend/06_fastapi_getting_started/01_fastapi_kickoff_core_concepts.md',
+        identifier_uuid: 'create_uuid',
+      },
+
+      'Basic API Features' => {
+        title: 'Basic API Features',
+        description: 'Basic API Features',
+        is_project: false,
+        github_path: '/curriculum2/02_python_backend/06_fastapi_getting_started/02_basic_api_features.md',
+        identifier_uuid: 'create_uuid',
+      },
+
+      'Database Integration' => {
+        title: 'Database Integration',
+        description: 'Database Integration',
+        is_project: false,
+        github_path: '/curriculum2/02_python_backend/06_fastapi_getting_started/04_advanced_features.md',
+        identifier_uuid: 'create_uuid',
+      },
+
+      'Advanced Features' => {
+        title: 'Advanced Features',
+        description: 'Advanced Features',
+        is_project: false,
+        github_path: '/curriculum2/02_python_backend/06_fastapi_getting_started/05_testing_deployment_capstone.md',
+        identifier_uuid: 'create_uuid',
+      },
+
+      'Testing and Deployment Capstone' => {
+        title: 'Testing and Deployment Capstone',
+        description: 'Testing and Deployment Capstone',
+        is_project: false,
+        github_path: '/curriculum2/02_python_backend/06_fastapi_getting_started/05_testing_deployment_capstone.md',
+        identifier_uuid: 'create_uuid',
+      },
+      'Inside Python' => {
+        title: 'Inside Python',
+        description: 'Inside Python',
+        is_project: false,
+        github_path: '/curriculum2/02_python_backend/01_inside_python.md',
+        identifier_uuid: 'create_uuid',
+      },
+
+      'Hands Behind Python' => {
+        title: 'Hands Behind Python',
+        description: 'Hands Behind Python',
+        is_project: false,
+        github_path: '/curriculum2/02_python_backend/02_hands_behind_python.md',
+        identifier_uuid: 'create_uuid',
+      },
+
+      'Python Toolbox' => {
+        title: 'Python Toolbox',
+        description: 'Python Toolbox',
+        is_project: false,
+        github_path: '/curriculum2/02_python_backend/03_python_toolbox.md',
+        identifier_uuid: 'create_uuid',
+      },
+
+      'Python Getting Started' => {
+        title: 'Python Getting Started',
+        description: 'Python Getting Started',
+        is_project: false,
+        github_path: '/curriculum2/02_python_backend/04_python_getting_started.md',
+        identifier_uuid: 'create_uuid',
+      },
+
+      'Level Up Python Exercises' => {
+        title: 'Level Up Python Exercises',
+        description: 'Level Up Python Exercises',
+        is_project: false,
+        github_path: '/curriculum2/02_python_backend/05_level_up_python_exercises.md',
+        identifier_uuid: 'create_uuid',
+      },
+
+      'Testing with unittest, mock, fixture' => {
+        title: 'Testing with unittest, mock, fixture',
+        description: 'Testing with unittest, mock, fixture',
+        is_project: false,
+        github_path: '/curriculum2/02_python_backend/07_testing_with_unittest_mock_fixture.md',
+        identifier_uuid: 'create_uuid',
+      },
+
+      'Python Project' => {
+        title: 'Python Project',
+        description: 'Python Project',
+        is_project: true,
+        github_path: '/curriculum2/02_python_backend/08_project.md',
+        identifier_uuid: 'create_uuid',
+      },
+
+      'MVT & Jinja Templating' => {
+        title: 'MVT & Jinja Templating',
+        description: 'MVT & Jinja Templating',
+        is_project: false,
+        github_path: '/curriculum2/02_python_backend/09_mvt_jinja_templating.md',
+        identifier_uuid: 'create_uuid',
+      },
+
+      'Django' => {
+        title: 'Django',
+        description: 'Django',
+        is_project: false,
+        github_path: '/curriculum2/02_python_backend/10_django.md',
+        identifier_uuid: 'create_uuid',
+      }
+
+    }
+  end
