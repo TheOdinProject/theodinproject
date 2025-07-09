bddf64ac23c21707145ec15a92dfdd2f7309eb75
Vivek <vivekjain.vj.2019@gmail.com>
2025-07-09 11:57:25 +0530

    adding lock feature

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -9,8 +9,6 @@ class LessonsController < ApplicationController
       Courses::MarkCompletedLessons.call(user: current_user, lessons: Array(@lesson))
     end
   end
-  def Mark_Complete
 
-  end
-
+  
 end
