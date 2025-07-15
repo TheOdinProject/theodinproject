93ed0ff32459dd3fc1c4bf747fd2e9ff0c2e4420
Josh Smith <jmsmith1018@gmail.com>
2025-07-14 19:44:16 -0600

    Add disconnect method to destroy slim select

--- app/javascript/controllers/multi_select_controller.js
+++ app/javascript/controllers/multi_select_controller.js
@@ -3,12 +3,15 @@ import SlimSelect from 'slim-select'
 
 export default class MultiSelectController extends Controller {
   connect () {
-    // eslint-disable-next-line no-new
-    new SlimSelect({
+    this.slimSelect = new SlimSelect({
       select: this.element,
       events: {
         addable: function (value) { return value }
       }
     })
+  }
+
+  disconnect () {
+    this.slimSelect.destroy()
   }
 }
