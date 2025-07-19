87d395d6daf7577cd6ea03dc5ccd63c2c32efb63
Dylan Williams <dw@easylodge.com>
2025-07-19 20:11:26 +1000

    Chore: Complete 7.1 framework default upgrade
    Resolves #4742

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,7 @@ Bundler.require(*Rails.groups)
 module Theodinproject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
-
-    # TODO: Can be removed once config.load_defaults is updated to 7.1
-    config.active_support.cache_format_version = 7.1
+    config.load_defaults 7.1
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
