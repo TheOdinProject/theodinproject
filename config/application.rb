a1f7f7e044501b2255a99a3c92fee746e126995f
Dylan Williams <dw@easylodge.com>
2025-08-01 17:44:09 +1000

    Is it me? Am I the problem?

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Theodinproject
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w[assets tasks generators])
 
     # Configuration for the application, engines, and railties goes here.
     #
