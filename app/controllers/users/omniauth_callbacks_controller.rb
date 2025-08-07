9ef0189fccc56f7cdc27c21cc73c1f5cde81dcc0
vivekjain2811 <vivekjain.vj.2019@gmail.com>
2025-08-07 23:35:44 +0530

    adding_mail_service_while_siging_up_through_google_and_github_auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  after_action :send_welcome_email, only: [:github, :google]
   def github
     @user = UserProvider.find_user(auth)
 
@@ -19,7 +20,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   private
+  def send_welcome_email
+    return unless  @user&.persisted? && @user.sign_in_count == 1 && ENV['STAGING'].nil?
 
+    UserMailer.send_welcome_email_to(@user).deliver_now
+  end
   def update_users_avatar
     @user.update!(avatar: avatar_from_provider)
   end
