4690ccd1be8ff99f6b422c5efd2a1d982e7056dd
vivekjain2811 <vivekjain.vj.2019@gmail.com>
2025-08-11 17:22:11 +0530

    removing_welcome_mail

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,12 +1,4 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  def create
-    super
-
-    if resource.persisted? && ENV['STAGING'].nil?
-      UserMailer.send_welcome_email_to(resource).deliver_later
-    end
-  end
-
   protected
 
   def after_sign_up_path_for(_resource)
