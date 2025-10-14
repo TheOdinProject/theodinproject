e7d533f643a338655f07715a90b4c62edc7a433d
Kevin <kevinmulhern23@gmail.com>
2025-10-14 23:28:21 +0100

    Chore: Use class variants for Tailwind form builder and flash component styles (#5102)
    Because:
    - We are replacing the classy_yaml gem with [class
    variants](https://github.com/avo-hq/class_variants?tab=readme-ov-file)
    
    Closes:
    - https://github.com/TheOdinProject/theodinproject/issues/5094
    - https://github.com/TheOdinProject/theodinproject/issues/5095
    
    This commit:
    - Installs the gem and makes use of it in the form builder and one of
    our components to prove it's working as expected.

--- app/components/overlays/flash_component.rb
+++ app/components/overlays/flash_component.rb
@@ -1,6 +1,23 @@
 class Overlays::FlashComponent < ApplicationComponent
   DISSALLOWED_TYPES = %i[timedout].freeze
 
+  FLASH_CLASSES = ClassVariants.build do
+    variant type: :notice do
+      slot :bg, class: 'bg-green-100'
+      slot :icon, class: 'text-green-700'
+      slot :text, class: 'text-green-900'
+      slot :button,
+           class: 'bg-green-100 text-green-600 hover:bg-green-200 focus:ring-green-600 focus:ring-offset-green-100'
+    end
+
+    variant type: :alert do
+      slot :bg, class: 'bg-red-100'
+      slot :icon, class: 'text-red-700'
+      slot :text, class: 'text-red-900'
+      slot :button, class: 'bg-red-100 text-red-600 hover:bg-red-200 focus:ring-red-600 focus:ring-offset-red-100'
+    end
+  end
+
   def initialize(type:, message:)
     @type = type.to_sym
     @message = message
@@ -15,6 +32,10 @@ class Overlays::FlashComponent < ApplicationComponent
       alert: 'icons/exclamation-circle-solid.svg',
       notice: 'icons/checkmark-circle-solid.svg',
     }.fetch(type)
+  end
+
+  def classes(slot)
+    FLASH_CLASSES.render(slot, type: type)
   end
 
   private
