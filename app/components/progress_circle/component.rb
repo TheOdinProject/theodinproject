e82deaa1632b96acba9fda7c524905e2b9fdc821
Kevin <kevinmulhern23@gmail.com>
2025-10-17 06:22:38 +0100

    Chore: Migrate progress circle component to class variants
    Because:
    - We previously used classy-yaml for style variants, but it is unmaintained and  incompatible with the latest versions of ViewComponents.
    
    This commit:
    - Improves the DSL of class variants in components by allowing them to be declared within a block that will not run up against our linting line length rules.
    - Adds a shared classes_for method to the application component to allow easy fetching of classes in all of our components.
    - Migrates progress circle styles to class variants.

--- app/components/progress_circle/component.rb
+++ app/components/progress_circle/component.rb
@@ -1,9 +1,27 @@
 class ProgressCircle::Component < ApplicationComponent
   renders_one :icon
 
+  style do
+    variant type: :default do
+      slot :text, class: 'text-xs sm:text-sm'
+      slot :container_size, class: 'h-24 w-24 sm:h-28 sm:w-28'
+      slot :icon_size, class: 'w-20 h-20 sm:p-2 sm:w-24 sm:h-24'
+    end
+
+    variant type: :small do
+      slot :text, class: 'text-xs p-2'
+      slot :container_size, class: 'h-24 w-24'
+      slot :icon_size, class: 'w-24 h-24 p-3'
+    end
+  end
+
   def initialize(percentage: 0, options: {})
     @percentage = percentage
     @options = options
+  end
+
+  def variant
+    size
   end
 
   private
