75750f937f3e3dda71693d5c1580de69270f278f
Kevin <kevinmulhern23@gmail.com>
2025-10-21 07:32:58 +0100

    Chore: Migrate vertical navigation item component styles to class variants
    Because:
    - We previously used classy-yaml for style variants, but it is unmaintained and incompatible with the latest versions of ViewComponents.
    
    This commit:
    - Migrates vertical navigation item styles to class variants.

--- app/components/vertical_navigation/item_component.rb
+++ app/components/vertical_navigation/item_component.rb
@@ -1,4 +1,17 @@
 class VerticalNavigation::ItemComponent < ApplicationComponent
+  style do
+    variant type: :active do
+      slot :link, class: 'bg-gray-200 text-gray-900 dark:bg-gray-700/50 dark:text-gray-300'
+      slot :icon, class: 'text-gray-500 group-hover:text-gray-700 dark:text-gray-300 dark:group-hover:text-gray-300'
+    end
+
+    variant type: :inactive do
+      slot :link,
+           class: 'text-gray-600 hover:bg-gray-50 hover:text-gray-900 dark:hover:bg-gray-600/70 dark:hover:text-gray-300 dark:text-gray-400' # rubocop:disable Layout/LineLength
+      slot :icon, class: 'text-gray-400 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-300'
+    end
+  end
+
   def initialize(name:, href:, icon_path:)
     @name = name
     @href = href
@@ -7,6 +20,10 @@ class VerticalNavigation::ItemComponent < ApplicationComponent
 
   def status
     current_page?(href) ? :active : :inactive
+  end
+
+  def variant
+    status
   end
 
   private
