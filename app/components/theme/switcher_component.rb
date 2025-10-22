f1e8d6530384d436a7b6f1e350d73e5a1bd98eba
Kevin <kevinmulhern23@gmail.com>
2025-10-22 22:39:30 +0100

    Chore: Migrate theme switcher component styles to class variants (#5141)
    Because:
    - We previously used classy-yaml for style variants, but it is
    unmaintained and incompatible with the latest versions of
    ViewComponents.
    - Closes https://github.com/TheOdinProject/theodinproject/issues/5097
    
    This commit:
    - Migrates theme switcher styles to class variants.

--- app/components/theme/switcher_component.rb
+++ app/components/theme/switcher_component.rb
@@ -1,7 +1,35 @@
 class Theme::SwitcherComponent < ApplicationComponent
+  style do
+    base do
+      slot :link, class: 'text-gray-600 group flex items-center dark:text-gray-300'
+      slot :icon,
+           class: 'h-5 w-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-300 dark:group-hover:text-gray-300'
+    end
+
+    variant type: :default do
+      slot :link, class: 'py-2 px-3'
+      slot :icon, class: 'mr-3'
+    end
+
+    variant type: :mobile do
+      slot :link,
+           class: 'text-gray-600 hover:bg-gray-50 hover:text-gray-900 text-base font-medium py-2 px-2 dark:hover:bg-gray-700/60 dark:hover:text-gray-200' # rubocop:disable Layout/LineLength
+      slot :icon, class: 'mr-4 h-6 w-6'
+    end
+
+    variant type: :icon_only do
+      slot :link, class: 'py-2 px-3'
+      slot :icon, class: 'text-gray-400 group-hover:text-gray-700'
+    end
+  end
+
   def initialize(current_theme:, type: :default)
     @current_theme = current_theme
     @type = type
+  end
+
+  def variant
+    type
   end
 
   def text
