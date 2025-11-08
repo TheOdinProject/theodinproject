98c9f0f83f92081258fb9046239a05bf0e17eae0
Dylan Williams <dw@easylodge.com>
2025-11-08 16:26:09 +1100

    Tweak new local CI DSL

--- config/ci.rb
+++ config/ci.rb
@@ -1,16 +1,18 @@
 # Run using bin/ci
 
 CI.run do
-  step 'Setup', 'bin/setup --skip-server'
+  # step 'Setup', 'bin/setup --skip-server'
 
   step 'Style: Ruby', 'bin/rubocop'
+  step 'Style: ERB',  'bin/erb_lint --lint-all'
+  step 'Style: JS', 'yarn lint'
+  step 'Style: CSS', 'yarn run stylelint'
 
-  step 'Security: Importmap vulnerability audit', 'bin/importmap audit'
   step 'Security: Brakeman code analysis', 'bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error'
 
-  step 'Tests: Rails', 'bin/rails test'
-  step 'Tests: System', 'bin/rails test:system'
-  step 'Tests: Seeds', 'env RAILS_ENV=test bin/rails db:seed:replant'
+  step 'Tests: Rails', 'bin/rspec --tag ~type:system'
+  step 'Tests: System', 'bin/rails spec:system'
+  # step 'Tests: Seeds', 'env RAILS_ENV=test bin/rails db:seed:replant'
 
   # Optional: set a green GitHub commit status to unblock PR merge.
   # Requires the `gh` CLI and `gh extension install basecamp/gh-signoff`.
