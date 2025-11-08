b77a6d94deb956693ce5e1593dc81ac0bad286fb
Dylan Williams <dw@easylodge.com>
2025-11-08 16:56:42 +1100

    Add todo comments to new CI DSL

--- config/ci.rb
+++ config/ci.rb
@@ -1,7 +1,7 @@
 # Run using bin/ci
 
 CI.run do
-  # step 'Setup', 'bin/setup --skip-server'
+  # step 'Setup', 'bin/setup --skip-server' # TODO: Needs some tweaking to run
 
   step 'Style: Ruby', 'bin/rubocop'
   step 'Style: ERB',  'bin/erb_lint --lint-all'
@@ -12,7 +12,7 @@ CI.run do
 
   step 'Tests: Rails', 'bin/rspec --tag ~type:system'
   step 'Tests: System', 'bin/rails spec:system'
-  # step 'Tests: Seeds', 'env RAILS_ENV=test bin/rails db:seed:replant'
+  # step 'Tests: Seeds', 'env RAILS_ENV=test bin/rails db:seed:replant' # TODO: Needs some tweaking to run
 
   # Optional: set a green GitHub commit status to unblock PR merge.
   # Requires the `gh` CLI and `gh extension install basecamp/gh-signoff`.
