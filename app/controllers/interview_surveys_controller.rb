90c282b292c5adbdb04712e20e0455930b351aae
Josh Smith <jmsmith1018@gmail.com>
2025-11-13 12:57:39 -0700

    Put front end of multi select form together

--- app/controllers/interview_surveys_controller.rb
+++ app/controllers/interview_surveys_controller.rb
@@ -1,12 +1,31 @@
 class InterviewSurveysController < ApplicationController
+  before_action :authenticate_user!
+  # TODO: merge feature flag stuff in
+
   def new
-    unless Feature.enabled?(:survey_feature, current_user)
+    if Flipper.enabled?(:survey_feature, current_user)
+      @interview_survey = InterviewSurvey.new
+    else
       redirect_to dashboard_path, notice: 'Feature not enabled'
     end
   end
 
   def create
-    # puts params[:survey]
-    redirect_to dashboard_path, notice: 'Survey Submitted'
+    @interview_survey = current_user.interview_surveys.build(interview_survey_params)
+
+    if @interview_survey.save
+      redirect_to dashboard_path, notice: 'Survey submitted'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def interview_survey_params
+    params.require(:interview_survey).permit(
+      :interview_date,
+      interview_concept_names: []
+    )
   end
 end
