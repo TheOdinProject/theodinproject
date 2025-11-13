a8d93112f45dab8591dce568e904022356e260b3
Josh Smith <jmsmith1018@gmail.com>
2025-11-13 12:40:42 -0700

    Add stimulus controller and styles for using `tom-select`

--- app/javascript/controllers/multi_select_controller.js
+++ app/javascript/controllers/multi_select_controller.js
@@ -2,14 +2,14 @@ import { Controller } from '@hotwired/stimulus'
 import TomSelect from 'tom-select'
 
 export default class MultiSelectController extends Controller {
-  connect() {
+  connect () {
     this.tomSelect = new TomSelect(
       this.element,
-      {}
+      { create: true }
     )
   }
 
-  disconnect() {
+  disconnect () {
     if (this.tomSelect) {
       this.tomSelect.destroy()
     }
