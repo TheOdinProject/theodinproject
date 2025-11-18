82a895894607948ee9fa8d3bcebbbe52d38dec0d
mao-sz <122839503+mao-sz@users.noreply.github.com>
2025-11-18 00:44:11 +0000

    Test lesson list content rendering

--- spec/components/lesson_list_component_spec.rb
+++ spec/components/lesson_list_component_spec.rb
@@ -10,10 +10,16 @@ RSpec.describe LessonListComponent, type: :component do
   end
 
   it 'renders the list of lessons' do
-    component = described_class.new(title: 'HTML Basics', lessons: [], current_user: nil)
+    count = rand(10)
+    component = described_class.new(
+      title: 'HTML Basics',
+      lessons: create_list(:lesson, count, title: 'Lesson title'),
+      current_user: nil
+    )
 
     render_inline(component)
 
     expect(page).to have_css('div[data-test-id="lesson-list"]')
+    expect(page).to have_content('Lesson title', count:)
   end
 end
