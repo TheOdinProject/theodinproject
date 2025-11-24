ab9cf38340e8a6ccfbb820d189d9ebeb77e2124e
Josh Smith <jmsmith1018@gmail.com>
2025-11-23 18:37:34 -0700

    Use `Feature` adaptor instead of calling `Flipper` directly

--- app/controllers/concerns/requires_feature.rb
+++ app/controllers/concerns/requires_feature.rb
@@ -2,7 +2,7 @@ module RequiresFeature
   def requires_feature(name, **)
     before_action(
       lambda do
-        return if Flipper.enabled?(name, current_user)
+        return if Feature.enabled?(name, current_user)
 
         redirect_to dashboard_path, notice: 'Feature not enabled'
       end,
