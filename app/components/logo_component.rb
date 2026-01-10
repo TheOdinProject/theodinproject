bb197c77eb2355700a847006212dcfb6f894621e
Josh Smith <jmsmith1018@gmail.com>
2026-01-10 11:19:47 -0700

    Fix: Navbar and LogoComponent: Eliminate duplicate header logos (#5209)
    The navbar duplicates a home link under the logo in the top-left. One of
    the links hides its logo at smaller screens and the other hides it at
    larger screens, but both links always remain in the navbar, which
    presents an accessibility issue and confusing tab navigation behavior.
    
    ### This PR:
    - Adds a slot to render a `small_icon` in the logo component.
    - Removes the duplicate link in the navbar and replaces with a
    `LogoComponent` render using the small_icon slot.
    
    ### Issue
    Closes #5200
    
    ### Additional Things
    Willing to change the style things. I figured it'd be nice to fully
    embed the display toggling when the small logo is present (so the small
    logo always has `"block lg:hidden"`, and the larger icon always has
    `"hidden lg:block"` when the small icon is there) because I figured
    there'd be no conceivable scenario where you'd want to display both of
    them at the same time. But I also realize it's adding some small
    behavior to the component that didn't really have much before. Would be
    glad to change to all styles coming from the outside if that's
    preferred.
    
    ### QA
    
    - Open any page and look at the navbar
    - Confirm that there's only one Home Link
    - At a tablet sized width just above the breakpoint to mobile, the logo
    should be small (without the 'Odin Project' text)
    - At a wide view, the logo should be wide (including the 'Odin Project'
    text)
    - Also potentially good to sanity check other `LogoComponent` renders
    (like the footer or the mobile sidenav menu) to make sure nothing is
    wrong with them (I don't see any issues)

--- app/components/logo_component.rb
+++ app/components/logo_component.rb
@@ -1,10 +1,18 @@
 class LogoComponent < ApplicationComponent
+  renders_one(:small_icon, lambda do |classes:|
+    image_tag 'icons/odin-icon.svg', alt: 'Odin Logo', class: "block lg:hidden #{classes}"
+  end)
+
   def initialize(classes:, path: nil)
     @classes = classes
     @path = path
   end
 
   attr_reader :classes
+
+  def before_render
+    @classes = "#{classes} hidden lg:block" if small_icon?
+  end
 
   def redirect_to_path
     @path || root_path
