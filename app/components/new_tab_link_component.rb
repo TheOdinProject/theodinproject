53b467afe1c902cfa3b752dad496179115f59f4b
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-04-14 21:17:37 +0100

    Create new tab link component

---
+++ app/components/new_tab_link_component.rb
@@ -0,0 +1,13 @@
+class NewTabLinkComponent < ApplicationComponent
+  def initialize(text:, href:, classes:, data: nil, noreferrer: true)
+    @text = text
+    @href = href
+    @classes = classes
+    @data = data
+    @noreferrer = noreferrer
+  end
+
+  private
+
+  attr_reader :text, :href, :classes, :data, :noreferrer
+end
---
+++ spec/components/new_tab_link_component_spec.rb
@@ -0,0 +1,78 @@
+require 'rails_helper'
+
+RSpec.describe NewTabLinkComponent, type: :component do
+  it 'renders the link with a new tab svg icon' do
+    component = described_class.new(
+      text: 'link text',
+      href: 'link href',
+      classes: 'a-class'
+    )
+
+    render_inline(component)
+
+    expect(page).to have_css('svg > desc', text: 'New tab')
+  end
+
+  it 'renders the link with an href' do
+    component = described_class.new(
+      text: 'link text',
+      href: 'link href',
+      classes: 'a-class'
+    )
+
+    render_inline(component)
+
+    expect(page).to have_css('[href="link href"]')
+  end
+
+  it 'renders the link with a text label' do
+    component = described_class.new(
+      text: 'link text',
+      href: 'link href',
+      classes: 'a-class'
+    )
+
+    render_inline(component)
+
+    expect(page).to have_link('link text')
+  end
+
+  it 'renders the link with target="_blank" and rel="noreferrer"' do
+    component = described_class.new(
+      text: 'link text',
+      href: 'link href',
+      classes: 'a-class'
+    )
+
+    render_inline(component)
+
+    expect(page).to have_css('[target="_blank"]')
+    expect(page).to have_css('[rel="noreferrer"]')
+  end
+
+  it 'renders the link with data attributes if provided' do
+    component = described_class.new(
+      text: 'link text',
+      href: 'link href',
+      classes: 'a-class',
+      data: { test_id: 'test-id' }
+    )
+
+    render_inline(component)
+
+    expect(page).to have_css('[data-test-id="test-id"]')
+  end
+
+  it 'omits rel attribute if noreferrer: false' do
+    component = described_class.new(
+      text: 'link text',
+      href: 'link href',
+      classes: 'a-class',
+      noreferrer: false
+    )
+
+    render_inline(component)
+
+    expect(page).to have_no_css('[rel]')
+  end
+end
