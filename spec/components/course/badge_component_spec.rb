74217778eda2c8628588a25a3a78aedf9b1d7305
Bryan Fines <49371751+BryanF1nes@users.noreply.github.com>
2026-06-07 06:54:56 -0400

    Feature: Course::BadgeComponent now has a linked parameter (#5380)
    <!-- Thank you for taking the time to contribute to The Odin Project. In
    order to get this pull request (PR) merged in a reasonable amount of
    time, you must complete this entire template. -->
    
    ## Because
    <!-- Summarize the purpose or reasons for this PR, e.g. what problem it
    solves or what benefit it provides. -->
    This PR updates the Course::BadgeComponent to have an additional
    `linked` param that can prevent the badge icon from being a clickable
    link on certain pages.
    
    ## This PR
    <!-- A bullet point list of one or more items describing the specific
    changes. -->
    - Added a `linked` param to the `Course::BadgeComponent` constructor and
    defaulted to `true`
    - Updated the `badge_component.html.erb` to account for a `linked?`
    param
    - Updated the `Course::BadgeComponent` in `course/_banner.html.erb` to a
    `false` linked state
    - Included two tests to ensure links are rendered accordingly based on
    the `linked` param
    
    ## Issue
    <!--
    If this PR closes an open issue in this repo, replace the XXXXX below
    with the issue number, e.g. Closes #2013.
    
    If this PR closes an open issue in another TOP repo, replace the #XXXXX
    with the URL of the issue, e.g. Closes
    https://github.com/TheOdinProject/curriculum/issues/XXXXX
    
    If this PR does not close, but is related to another issue or PR, you
    can link it as above without the 'Closes' keyword, e.g. 'Related to
    #2013'.
    -->
    Closes #5330 
    
    ## Additional Information
    <!-- Any other information about this PR, such as a link to a Discord
    discussion. -->
    
    
    ## Pull Request Requirements
    <!-- Replace the whitespace between the square brackets with an 'x',
    e.g. [x]. After you create the PR, they will become checkboxes that you
    can click on. -->
    - [x] I have thoroughly read and understand [The Odin Project
    Contributing
    Guide](https://github.com/TheOdinProject/theodinproject/blob/main/CONTRIBUTING.md)
    - [x] The title of this PR follows the `keyword: brief description of
    change` format, using one of the following keywords:
        - `Feature` - adds new or amends existing user-facing behavior
    - `Chore` - changes that have no user-facing value, refactors,
    dependency bumps, etc
        - `Fix` - bug fixes
    -   [x] The `Because` section summarizes the reason for this PR
    - [x] The `This PR` section has a bullet point list describing the
    changes in this PR
    - [x] I have verified all tests and linters pass after making these
    changes.
    - [x] If this PR addresses an open issue, it is linked in the `Issue`
    section
    -   [x] If applicable, this PR includes new or updated automated tests

--- spec/components/course/badge_component_spec.rb
+++ spec/components/course/badge_component_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Course::BadgeComponent, type: :component do
+  include Rails.application.routes.url_helpers
+
   context 'when the user is signed in' do
     it 'renders the loading progress circle' do
       course = create(:course)
@@ -22,6 +24,28 @@ RSpec.describe Course::BadgeComponent, type: :component do
       render_inline(component)
 
       expect(page).to have_css("[data-test-id='default-badge']")
+    end
+  end
+
+  context 'when linked is true' do
+    it 'renders a link to the course badge' do
+      course = create(:course)
+      component = described_class.new(course:, linked: true)
+
+      render_inline(component)
+
+      expect(page).to have_link(href: path_course_path(course.path, course))
+    end
+  end
+
+  context 'when linked is false' do
+    it 'does not render a link to the course badge' do
+      course = create(:course)
+      component = described_class.new(course:, linked: false)
+
+      render_inline(component)
+
+      expect(page).to have_no_link(href: path_course_path(course.path, course))
     end
   end
 end
